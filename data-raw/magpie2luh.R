4697be2a2ac7326de3b259b7bc7b4c4311a0916e
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-04-11 10:12:30 +0200

    move LUHofMAG to sysdata.rda

---
+++ data-raw/magpie2luh.R
@@ -0,0 +1,11 @@
+# following the workflow proposed by https://r-pkgs.org/data.html#sec-data-sysdata
+# code to prepare the internal dataset `magpie2luh` which maps magpie crops to LUH crop categories
+withr::with_package("mrcommons", {
+  # TODO this is just a rough approximation, also using shares from 2010 is arbitrary
+  magpie2luh <- madrat::calcOutput(type = "LUH2MAgPIE", aggregate = FALSE, share = "LUHofMAG",
+                                   bioenergy = "fix", missing = "fill", rice = "total")
+})
+magpie2luh <- magpie2luh[, 2010, ]
+magclass::getYears(magpie2luh) <- NULL
+
+usethis::use_data(magpie2luh, overwrite = TRUE)
