bf92743c22a5386fc801b31a056efd84d622f365
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-04-12 15:42:24 +0200

    magpie2luh -> magpieCropsToLuh

--- data-raw/magpieCropsToLuh.R
+++ data-raw/magpieCropsToLuh.R
@@ -1,11 +1,11 @@
 # following the workflow proposed by https://r-pkgs.org/data.html#sec-data-sysdata
-# code to prepare the internal dataset `magpie2luh` which maps magpie crops to LUH crop categories
+# code to prepare the internal dataset `magpieCropsToLuh` which maps magpie crops to LUH crop categories
 withr::with_package("mrcommons", {
   # TODO this is just a rough approximation, also using shares from 2010 is arbitrary
-  magpie2luh <- madrat::calcOutput(type = "LUH2MAgPIE", aggregate = FALSE, share = "LUHofMAG",
+  magpieCropsToLuh <- madrat::calcOutput(type = "LUH2MAgPIE", aggregate = FALSE, share = "LUHofMAG",
                                    bioenergy = "fix", missing = "fill", rice = "total")
 })
-magpie2luh <- magpie2luh[, 2010, ]
-magclass::getYears(magpie2luh) <- NULL
+magpieCropsToLuh <- magpieCropsToLuh[, 2010, ]
+magclass::getYears(magpieCropsToLuh) <- NULL
 
-usethis::use_data(magpie2luh, internal = TRUE, overwrite = TRUE)
+usethis::use_data(magpieCropsToLuh, internal = TRUE, overwrite = TRUE)
