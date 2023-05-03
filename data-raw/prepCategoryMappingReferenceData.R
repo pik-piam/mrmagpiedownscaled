72d6e1f3ecc43b78903a70b0f0e3060263d6ef92
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2023-05-03 15:52:38 +0200

    added data for remapping of categories

---
+++ data-raw/prepCategoryMappingReferenceData.R
@@ -0,0 +1,17 @@
+# following the workflow proposed by https://r-pkgs.org/data.html#sec-data-sysdata
+# code to prepare the internal datasets for category mapping
+withr::with_package("mrcommons", {
+  fao <- collapseDim(madrat::readSource("FAO_online", "Crop")[,2019 , "area_harvested"])
+})
+
+saveRDS(fao, file = "inst/extdata/faoAreaHarvested2019.rds")
+
+# get country geometry
+withr::with_package("mrorganic", {
+  countries <- calcOutput("WorldCountries", aggregate = FALSE)
+  geometryCountries <- attr(as.magpie(countries), "geometry")
+})
+
+saveRDS(geometryCountries, file = "inst/extdata/geometryCountries.rds")
+
+
