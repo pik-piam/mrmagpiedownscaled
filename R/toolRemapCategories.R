676b59559fcd0b5269fe3200f43f06f1428063c5
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2023-05-04 10:36:51 +0200

    converted toolRemapCategories into envisioned structure (not yet ready)

--- R/toolRemapCategories.R
+++ R/toolRemapCategories.R
@@ -3,13 +3,26 @@
 toolRemapCategories <- function(x, input2fao, output2fao) {
 
   # prepare FAO reference data
-  fao <- readRDS(system.file("extdata/faoAreaHarvested2019.rds", package = "mrdownscale"))
-  geometryCountries <- readRDS(system.file("extdata/geometryCountries.rds", package = "mrdownscale"))
-  attr(fao, "geometry") <- geometryCountries
-  vFao <- as.SpatVector(fao)
+  .getFaoSpatVector <- function() {
+    fao <- readRDS(system.file("extdata/faoAreaHarvested2019.rds", package = "mrdownscale"))
+    geometryCountries <- readRDS(system.file("extdata/geometryCountries.rds", package = "mrdownscale"))
+    attr(fao, "geometry") <- geometryCountries
+    return(as.SpatVector(fao))
+  }
+  fao <- .getFaoSpatVector()
 
+  # project fao and luh2 data on x
+  # ToDo write projectData function
+  fao  <- projectData(fao, x)
+  luh2 <- projectData(luh2, x)
 
-  # dataFao <- toolAggregate(x, input2fao, weight = faoArea)
-  # dataOut <- toolAggregate(dataFao, output2fao)
-  return(dataOut)
+  mluh2 <- as.magpie(luh2)
+  mfao <- as.magpie(fao)
+  ref <- mbind(mfao, mluh2)
+
+  mx   <- as.magpie(x)
+
+  xRef <- toolAggregate(mx, input2ref, weight = ref)
+  xOut <- toolAggregate(xRef, output2ref)
+  return(xOut)
 }
