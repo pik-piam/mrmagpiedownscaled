9c8ba1c9d5fbe8adc15c6cf8d0feb33bdf395261
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-05-11 17:35:41 +0200

    reformat luh for harmonize

--- R/calcHarmonized.R
+++ R/calcHarmonized.R
@@ -2,8 +2,15 @@ calcHarmonized <- function() {
   magpie <- readSource("Magpie") # TODO change to calcOutput("HarmonizedCategories")
   # TODO reformat magpie to fit harmonized interface
 
-  luh <- calcOutput("LowResLUH2v2h", aggregate = FALSE)
-  # TODO reformat luh to fit harmonized interface
+  luhVector <- calcOutput("LowResLUH2v2h", aggregate = FALSE)
+  stopifnot(identical(luhVector[["cluster", drop = TRUE]], 1:200))
+  luh <- NULL
+  for (colName in grep("cluster", names(luhVector), value = TRUE, invert = TRUE)) {
+    year <- as.integer(strsplit(colName, "..", fixed = TRUE)[[1]][[1]])
+    category <- strsplit(colName, "..", fixed = TRUE)[[1]][[2]]
+    luh <- rbind(luh, data.frame(cluster = 1:200, year = year, category = category,
+                             .value = luhVector[[colName, drop = TRUE]]))
+  }
 
   harmonized <- mip::harmonize(magpie, luh, finalYear = "2050", harmonizeYear = "1995", method = "ratio")
   # TODO turn harmonized dataframe into SpatVector
