4629e7447fae84a5d6e31266ee8445371453cc78
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2023-05-11 09:23:12 +0200

    bugfixes

--- R/toolRemapCategories.R
+++ R/toolRemapCategories.R
@@ -6,7 +6,8 @@ toolRemapCategories <- function(x, input2fao, output2fao) {
   .getFaoSpatVector <- function() {
     fao <- readRDS(system.file("extdata/faoAreaHarvested2019.rds", package = "mrdownscale"))
     geometryCountries <- readRDS(system.file("extdata/geometryCountries.rds", package = "mrdownscale"))
-    attr(fao, "geometry") <- geometryCountries
+    attr(fao, "geometry") <- geometryCountries[getItems(fao, dim = 1)]
+    attr(fao, "crs") <- "+proj=longlat +datum=WGS84 +no_defs"
     return(as.SpatVector(fao))
   }
   fao  <- .getFaoSpatVector()
@@ -29,15 +30,19 @@ toolRemapCategories <- function(x, input2fao, output2fao) {
 }
 
 projectData <- function(x, target) {
-  # TODO: need to divide x values by polygon area!!
-  # something like:
-  # x2 <- x/terra::expanse(x, unit = "ha")
+  elementSize <- terra::expanse(x, unit = "ha")
+  for(i in which(terra::datatype(x) == "double")) {
+    x[[i]] <- x[[i]]/elementSize
+  }
   out <- list()
   for(i in seq_len(dim(target)[1])) {
     out[[i]] <- terra::intersect(target[i,], x)
   }
   out <- do.call(rbind, out)
-  # TODO: need to multiply x values by polygon area!!
+  elementSize <- terra::expanse(out, unit = "ha")
+  for(i in which(terra::datatype(out) == "double")) {
+    out[[i]] <- out[[i]] * elementSize
+  }
   out <- terra::aggregate(out, by ="clusterId", fun = "sum")
   return(out)
 }
