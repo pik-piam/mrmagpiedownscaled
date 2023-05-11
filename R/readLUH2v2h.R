a8137e55f41d645ccf988f964b9842e791f5fec5
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2023-05-11 14:34:27 +0200

    bugfixes

--- R/readLUH2v2h.R
+++ R/readLUH2v2h.R
@@ -5,11 +5,12 @@ readLUH2v2h <- function(subset = "1995|2000|2005|2010|2015") {
   x <- x[[grep("secm[ab]", names(x), invert = TRUE, value = TRUE)]]
 
   # fix years in names
-  names(x) <- paste0(sub("_.+", "", names(x)), "_", terra::time(x))
+  names(x) <- paste0(terra::time(x),"..",sub("_.+", "", names(x)))
 
-  x <- x[subset]
+  x <- x[as.character(subset)]
 
   return(list(x = x,
               class = "SpatRaster",
+              cache = FALSE,
               unit = "1"))
 }
