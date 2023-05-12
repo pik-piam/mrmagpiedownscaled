cb39c2b8e10739551142c829ba5c1691962694a2
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2023-05-12 14:59:14 +0200

    calcHarmonizedCategories does now produce a data set

--- R/calcHarmonizedCategories.R
+++ R/calcHarmonizedCategories.R
@@ -1,23 +1,22 @@
 calcHarmonizedCategories <- function(input = "magpie", output = "luh2") {
   if (input == "magpie") {
     x <- madrat::readSource("Magpie")
-    input2fao <- toolGetMapping("magpie2ref.csv", where = "mrdownscale")
+    input2ref <- toolGetMapping("magpie2ref.csv", where = "mrdownscale")
   } else {
     stop("Unsupported input type \"", input, "\"")
   }
 
   if (output == "luh2") {
-    output2fao <- toolGetMapping("luh2ref.csv", where = "mrdownscale")
+    output2ref <- toolGetMapping("luh2ref.csv", where = "mrdownscale")
   } else {
     stop("Unsupported output type \"", output, "\"")
   }
 
-  x <- toolRemapCategories(x, input2fao, output2fao)
+  x <- toolRemapCategories(x, input2ref, output2ref)
 
   return(list(x = x,
               class = "SpatVector",
               isocountries = FALSE,
               unit = "Mha",
-              min = 0,
               description = "Input data with land categories remapped to categories of output target data set"))
 }
