4b9a417a4686feeb25e36fea68709f0e5d5f0bef
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2023-05-12 14:59:14 +0200

    calcHarmonizedCategories does now produce a data set

--- R/readLUH2v2h.R
+++ R/readLUH2v2h.R
@@ -5,7 +5,7 @@ readLUH2v2h <- function(subset = "1995|2000|2005|2010|2015") {
   x <- x[[grep("secm[ab]", names(x), invert = TRUE, value = TRUE)]]
 
   # fix years in names
-  names(x) <- paste0(terra::time(x),"..",sub("_.+", "", names(x)))
+  names(x) <- paste0(terra::time(x), "..", sub("_.+", "", names(x)))
 
   x <- x[as.character(subset)]
 
--- R/toolRemapCategories.R
+++ R/toolRemapCategories.R
@@ -1,53 +1,61 @@
 
 
-toolRemapCategories <- function(x, input2fao, output2fao) {
+toolRemapCategories <- function(x, input2ref, output2ref) {
 
-  # prepare FAO reference data
-  .getFaoSpatVector <- function() {
-    fao <- readRDS(system.file("extdata/faoAreaHarvested2019.rds", package = "mrdownscale"))
-    geometryCountries <- readRDS(system.file("extdata/geometryCountries.rds", package = "mrdownscale"))
-    attr(fao, "geometry") <- geometryCountries[getItems(fao, dim = 1)]
-    attr(fao, "crs") <- "+proj=longlat +datum=WGS84 +no_defs"
-    return(as.SpatVector(fao))
-  }
-  fao      <- .getFaoSpatVector()
-  fao[[1]] <- NULL # remove country column
-  luh2 <- as.SpatRaster(read.magpie(system.file("extdata/luh2015.mz", package = "mrdownscale")))
-
-  # project fao and luh2 data on x
-  # ToDo write projectData function
-  pfao  <- projectData(fao, x[,1:2])
-  pluh2 <- projectData(luh2, x[,1:2])
-
-  mluh2 <- as.magpie(pluh2,  spatial = which(terra::datatype(mluh2) != "double"))
-  mfao <- as.magpie(pfao, spatial = which(terra::datatype(pfao) != "double"))
-  ref <- mbind(mfao, mluh2)
-
-  mx   <- as.magpie(x)
-
-  xRef <- toolAggregate(mx, input2ref, weight = ref)
-  xOut <- toolAggregate(xRef, output2ref)
-  return(xOut)
-}
-
-projectData <- function(x, target) {
-  if(inherits(x, "SpatVector")) {
+  .projectData <- function(x, target) {
+    if (inherits(x, "SpatVector")) {
       elementSize <- terra::expanse(x, unit = "ha")
-      for(i in which(terra::datatype(x) == "double")) {
-        x[[i]] <- x[[i]]/elementSize
+      for (i in which(terra::datatype(x) == "double")) {
+        x[[i]] <- x[[i]] / elementSize
       }
       out <- list()
-      for(i in seq_len(dim(target)[1])) {
-        out[[i]] <- terra::intersect(target[i,], x)
+      for (i in seq_len(dim(target)[1])) {
+        out[[i]] <- terra::intersect(target[i, ], x)
       }
       out <- do.call(rbind, out)
       elementSize <- terra::expanse(out, unit = "ha")
-      for(i in which(terra::datatype(out) == "double")) {
+      for (i in which(terra::datatype(out) == "double")) {
         out[[i]] <- out[[i]] * elementSize
       }
-      out <- terra::aggregate(out, by ="clusterId", fun = "sum", count = FALSE)
-  } else if(inherits(x, "SpatRaster")) {
-    out  <- terra::extract(x, target, "sum", bind = TRUE, na.rm=TRUE)
+      out <- terra::aggregate(out, by = "clusterId", fun = "sum", count = FALSE)
+    } else if (inherits(x, "SpatRaster")) {
+      out  <- terra::extract(x, target, "sum", bind = TRUE, na.rm = TRUE)
+    }
+    return(out)
   }
-  return(out)
+
+  # prepare FAO reference data
+  .getFaoSpatVector <- function() {
+    fao <- readRDS(system.file("extdata/faoAreaHarvested.rds", package = "mrdownscale"))
+    geometryCountries <- readRDS(system.file("extdata/geometryCountries.rds", package = "mrdownscale"))
+    attr(fao, "geometry") <- geometryCountries[getItems(fao, dim = 1)]
+    attr(fao, "crs") <- "+proj=longlat +datum=WGS84 +no_defs"
+    fao <- as.SpatVector(fao)
+    fao[[1]] <- NULL # remove country column
+    return(fao)
+  }
+  .getLUH2SpatRaster <- function() {
+    luh2 <- as.SpatRaster(read.magpie(system.file("extdata/luh2015.mz", package = "mrdownscale")))
+    return(luh2)
+  }
+  fao      <- .getFaoSpatVector()
+  luh2     <- .getLUH2SpatRaster()
+
+  # project fao and luh2 data on x
+  pfao  <- .projectData(fao, x[, 1:2])
+  pluh2 <- .projectData(luh2, x[, 1:2])
+
+  # convert to magclass
+  mluh2 <- as.magpie(pluh2,  spatial = which(terra::datatype(pluh2) != "double"))
+  mfao  <- as.magpie(pfao, spatial = which(terra::datatype(pfao) != "double"))
+  mx    <- as.magpie(x, spatial = which(terra::datatype(x) != "double"))
+  # harmonize reference data to million ha and merge
+  getItems(mfao, dim = 3) <- sub("^sum_", "", getItems(mfao, dim = 3))
+  ref <- mbind(mluh2, mfao * 10^-6)
+
+  xRef <- toolAggregate(mx[, , c("crop", "begr", "betr"), invert = TRUE], input2ref, dim = 3, weight = ref + 10^-10)
+  xOut <- toolAggregate(xRef, dim = 3, output2ref)
+  attr(xOut, "crs") <- attr(mx, "crs")
+  attr(xOut, "geometry") <- attr(mx, "geometry")
+  return(as.SpatVector(xOut))
 }
