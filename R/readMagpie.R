69a656596f9178e1795d9e9dfd620d3d6283c6f7
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-05-17 16:27:15 +0200

    Revert "use wide format in readMagpie"
    This reverts commit 628191c31a00641bc01fd480228b9f4ce7819aa7.

--- R/readMagpie.R
+++ R/readMagpie.R
@@ -7,16 +7,7 @@ readMagpie <- function() {
   cropArea   <- magpie4::croparea("fulldata.gdx", level = "cell", product_aggr = FALSE)
 
   x <- magclass::mbind(landUse, cropArea)
+  x <- magpie4::clusterOutputToTerraVector(x, clustermap)
 
-  # TODO get geom without using magpie4::clusterOutputToTerraVector
-  longFormat <- magpie4::clusterOutputToTerraVector(x, clustermap)
-  longFormat <- longFormat[!duplicated(longFormat$clusterId), ]
-  attr(x, "geometry") <- terra::geom(longFormat, wkt = TRUE)
-
-  x <- as.SpatVector(x)
-  stopifnot(identical(names(x)[1:2], c(".j", ".region")))
-  x <- x[, -1]
-  names(x)[1] <- "clusterId"
-  terra::crs(x) <- terra::crs(longFormat)
   return(list(x = x, class = "SpatVector"))
 }
