a67094e6c0f43d1818c4f5480d075ffbdf7d2572
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-05-23 10:06:50 +0200

    harmonization method fade

--- R/calcHarmonized.R
+++ R/calcHarmonized.R
@@ -1,4 +1,5 @@
-calcHarmonized <- function(input = "magpie", target = "luh2") {
+calcHarmonized <- function(input = "magpie", target = "luh2",
+                           harmonizeYear = 1995, finalYear = 2015, method = "fade") {
   input <- calcOutput("HarmonizedCategories", input = input, target = target, aggregate = FALSE)
 
   # get target data
@@ -12,36 +13,47 @@ calcHarmonized <- function(input = "magpie", target = "luh2") {
   ref    <- as.SpatVector(input[, , 1])[, 1:2]
   target <- terra::extract(target, ref, sum, na.rm = TRUE, bind = TRUE)
   target <- as.magpie(target)
+  stopifnot(setequal(getItems(input, 3), getItems(target, 3)))
+  target <- target[, , getItems(input, 3)] # harmonize order of dim 3
 
-  .df <- function(input) {
-    df <- magclass::as.data.frame(input, rev = 3)
-    stopifnot(identical(names(df), c("region", "id", "year", "data", ".value")))
-    df <- df[, -1]
-    names(df) <- c("region", "period", "variable", "value")
-    df$region <- as.character(df$region)
-    df$scenario <- "scenario"
-    df$model <- "MAgPIE"
-    df$unit <- "Mha"
-    return(df)
+  if (method == "offset") {
+    .df <- function(input) {
+      df <- magclass::as.data.frame(input, rev = 3)
+      stopifnot(identical(names(df), c("region", "id", "year", "data", ".value")))
+      df <- df[, -1]
+      names(df) <- c("region", "period", "variable", "value")
+      df$region <- as.character(df$region)
+      df$scenario <- "scenario"
+      df$model <- "model"
+      df$unit <- "Mha"
+      return(df)
+    }
+    dInput  <- .df(input)
+    dTarget <- .df(target)
+
+    out <- mip::harmonize(dInput, dTarget,
+                          harmonizeYear = as.character(harmonizeYear), finalYear = as.character(finalYear),
+                          method = "offset")
+
+    out <- as.magpie(out, spatial = "region", temporal = "period")
+    getSets(out)[1] <- "id"
+    spatElems <- getItems(input, dim = 1.1, full = TRUE)
+    names(spatElems) <- getItems(input, dim = "id", full = TRUE)
+    getItems(out, dim = "region", maindim = 1) <- unname(spatElems[getItems(out, dim = 1)])
+    out <- collapseDim(magpiesort(dimOrder(out, 2:1, dim = 1)))
+    getSets(out) <- c("region", "id", "year", "data")
+  } else if (method == "fade") {
+    out <- toolHarmonizeFade(input, target, harmonizeYear = harmonizeYear, finalYear = finalYear)
+  } else {
+    stop("Unexpected harmonization method: ", method)
   }
-  dInput  <- .df(input)
-  dTarget <- .df(target)
 
-  out <- mip::harmonize(dInput, dTarget, harmonizeYear = "1995", finalYear = "2040", method = "offset")
-
-  out <- as.magpie(out, spatial = "region", temporal = "period")
-  getSets(out)[1] <- "id"
-  spatElems <- getItems(input, dim = 1.1, full = TRUE)
-  names(spatElems) <- getItems(input, dim = "id", full = TRUE)
-  getItems(out, dim = "region", maindim = 1) <-  unname(spatElems[getItems(out, dim = 1)])
-  out <- collapseDim(magpiesort(dimOrder(out, 2:1, dim = 1)))
-  getSets(out) <- c("region", "id", "year", "data")
   attr(out, "geometry") <- attr(input, "geometry")
   attr(out, "crs")      <- attr(input, "crs")
 
   testthat::test_that("data fullfills format requirement", {
     testthat::expect_identical(unname(getSets(out)), c("region", "id", "year", "data"))
-    # testthat::expect_true(all(out >= 0)) <-- currently not fullfilled!
+    testthat::expect_true(all(out >= 0))
 
     # check for expected land categories
     testthat::expect_setequal(getItems(out, dim = 3), getItems(input, dim = 3))
---
+++ R/toolHarmonizeFade.R
@@ -0,0 +1,29 @@
+toolHarmonizeFade <- function(input, target, harmonizeYear, finalYear) {
+  inputYears <- getYears(input, as.integer = TRUE)
+  targetYears <- getYears(target, as.integer = TRUE)
+  stopifnot(round(harmonizeYear) == harmonizeYear,
+            round(finalYear) == finalYear,
+            finalYear > harmonizeYear,
+            harmonizeYear %in% inputYears,
+            harmonizeYear %in% targetYears,
+            finalYear %in% inputYears,
+            inputYears[inputYears < finalYear] %in% targetYears,
+            all.equal(getItems(input, dim = 1), getItems(target, dim = 1)),
+            all.equal(getItems(input, dim = 3), getItems(target, dim = 3)))
+  years <- sort(union(inputYears, targetYears))
+  stopifnot(years[years < finalYear] %in% targetYears,
+            years[years > harmonizeYear] %in% inputYears)
+  .inputFactor <- function(year) {
+    fac <- (year - harmonizeYear) / (finalYear - harmonizeYear)
+    stopifnot(0 < fac, fac < 1)
+    return(fac)
+  }
+  out <- target
+  for (year in years[years > harmonizeYear & years < finalYear]) {
+    out[, year, ] <- .inputFactor(year) * input[, year, ] + (1 - .inputFactor(year)) * target[, year, ]
+  }
+  yearsAfterHarmonization <- years[years >= finalYear]
+  out <- out[, yearsAfterHarmonization, , invert = TRUE]
+  out <- mbind(out, input[, yearsAfterHarmonization, ])
+  return(out)
+}
