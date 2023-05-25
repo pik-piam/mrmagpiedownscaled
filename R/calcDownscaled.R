abaee549d82ea8871588c879b66baf289b455c13
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2023-05-25 09:44:30 +0200

    bugfixes + added tests

--- R/calcDownscaled.R
+++ R/calcDownscaled.R
@@ -1,25 +1,25 @@
 calcDownscaled <- function(input = "magpie", target = "luh2") {
-  input <- calcOutput("Harmonized", input = input, target = target, aggregate = FALSE)
+  x <- calcOutput("Harmonized", input = input, target = target, aggregate = FALSE)
 
   # get target data
   if (target == "luh2") {
-    target <- madrat::readSource("LUH2v2h")
+    xTarget <- madrat::readSource("LUH2v2h")
   } else {
     stop("Unsupported output type \"", target, "\"")
   }
 
-  .getDownscaleMap <- function(x, target) {
+  .getDownscaleMap <- function(x, xTarget) {
     tmp <- as.SpatVector(x[, 1, 1])[, ".id"]
     tmp[[1]]$.id <- as.integer(tmp[[1]]$.id)
-    tmp <- terra::rasterize(tmp, target, ".id", touches = TRUE)
+    tmp <- terra::rasterize(tmp, xTarget, ".id", touches = TRUE)
     names(tmp) <- "id"
     tmp <- as.magpie(tmp)
     map <- data.frame(cell = getItems(tmp, dim = 1), cluster = as.vector(tmp))
     return(map)
   }
 
-  map <- .getDownscaleMap(input, target)
-  length(intersect(getItems(input, dim = 1), map$cell))
+  map <- .getDownscaleMap(x, xTarget)
+  xlength(intersect(getItems(input, dim = 1), map$cell))
 
   return(return(list(x = NULL,
                      class = "magpie",
