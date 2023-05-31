d8a727434858db39b70bc1e6ab86dee2922f7411
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-05-31 10:20:38 +0200

    Revert "WIP calcSecma"
    This reverts commit 8022e76e8f681471bb3b32418f9da9fb01f87bc3.

--- R/readMagpie.R
+++ R/readMagpie.R
@@ -1,31 +1,20 @@
-readMagpie <- function(subtype = "default") {
+readMagpie <- function() {
   "!# @monitor magpie4:::addGeometry"
 
-  if (subtype == "default") {
-    stopifnot(file.exists("fulldata.gdx"),
-              length(Sys.glob("clustermap_*.rds")) == 1)
+  stopifnot(file.exists("fulldata.gdx"),
+            length(Sys.glob("clustermap_*.rds")) == 1)
 
-    clustermap <- readRDS(Sys.glob("clustermap_*.rds"))
-    landUse    <- magpie4::land("fulldata.gdx", level = "cell")
-    cropArea   <- magpie4::croparea("fulldata.gdx", level = "cell", product_aggr = FALSE)
+  clustermap <- readRDS(Sys.glob("clustermap_*.rds"))
+  landUse    <- magpie4::land("fulldata.gdx", level = "cell")
+  cropArea   <- magpie4::croparea("fulldata.gdx", level = "cell", product_aggr = FALSE)
 
-    x <- magclass::mbind(landUse, cropArea)
-    x <- x[, , "crop", invert = TRUE] # remove crop to avoid double counting of areas
-    x <- magpie4::addGeometry(x, clustermap)
-    getSets(x) <- c("region", "id", "year", "data") # fix spatial set names
+  x <- magclass::mbind(landUse, cropArea)
+  x <- x[, , "crop", invert = TRUE] # remove crop to avoid double counting of areas
+  x <- magpie4::addGeometry(x, clustermap)
+  getSets(x) <- c("region", "id", "year", "data") # fix spatial set names
 
-    return(list(x = x,
-                class = "magpie",
-                unit = "Mha",
-                description = "Land cover information computed by MAgPIE"))
-  } else if (subtype == "ageclasses") {
-    x <- magpie4::land("fulldata.gdx", level = "cell", subcategories = c("other", "secdforest"))
-    x <- x[, , "total", invert = TRUE]
-    return(list(x = x,
-                class = "magpie",
-                unit = "Mha",
-                description = "Age classes land cover information computed by MAgPIE"))
-  } else {
-    stop("subtype ", subtype, " not allowed in readMagpie")
-  }
+  return(list(x = x,
+              class = "magpie",
+              unit = "Mha",
+              description = "Land cover information computed by MAgPIE"))
 }
