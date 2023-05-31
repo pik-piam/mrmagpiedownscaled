53462cc31320cf06e453bfb9b1643d63d56479c6
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-05-31 10:20:06 +0200

    WIP calcSecma

--- R/readMagpie.R
+++ R/readMagpie.R
@@ -1,20 +1,31 @@
-readMagpie <- function() {
+readMagpie <- function(subtype = "default") {
   "!# @monitor magpie4:::addGeometry"
 
-  stopifnot(file.exists("fulldata.gdx"),
-            length(Sys.glob("clustermap_*.rds")) == 1)
+  if (subtype == "default") {
+    stopifnot(file.exists("fulldata.gdx"),
+              length(Sys.glob("clustermap_*.rds")) == 1)
 
-  clustermap <- readRDS(Sys.glob("clustermap_*.rds"))
-  landUse    <- magpie4::land("fulldata.gdx", level = "cell")
-  cropArea   <- magpie4::croparea("fulldata.gdx", level = "cell", product_aggr = FALSE)
+    clustermap <- readRDS(Sys.glob("clustermap_*.rds"))
+    landUse    <- magpie4::land("fulldata.gdx", level = "cell")
+    cropArea   <- magpie4::croparea("fulldata.gdx", level = "cell", product_aggr = FALSE)
 
-  x <- magclass::mbind(landUse, cropArea)
-  x <- x[, , "crop", invert = TRUE] # remove crop to avoid double counting of areas
-  x <- magpie4::addGeometry(x, clustermap)
-  getSets(x) <- c("region", "id", "year", "data") # fix spatial set names
+    x <- magclass::mbind(landUse, cropArea)
+    x <- x[, , "crop", invert = TRUE] # remove crop to avoid double counting of areas
+    x <- magpie4::addGeometry(x, clustermap)
+    getSets(x) <- c("region", "id", "year", "data") # fix spatial set names
 
-  return(list(x = x,
-              class = "magpie",
-              unit = "Mha",
-              description = "Land cover information computed by MAgPIE"))
+    return(list(x = x,
+                class = "magpie",
+                unit = "Mha",
+                description = "Land cover information computed by MAgPIE"))
+  } else if (subtype == "ageclasses") {
+    x <- magpie4::land("fulldata.gdx", level = "cell", subcategories = c("other", "secdforest"))
+    x <- x[, , "total", invert = TRUE]
+    return(list(x = x,
+                class = "magpie",
+                unit = "Mha",
+                description = "Age classes land cover information computed by MAgPIE"))
+  } else {
+    stop("subtype ", subtype, " not allowed in readMagpie")
+  }
 }
