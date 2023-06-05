917148f425f471a9a66ea09fa8d40c1897f277be
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-06-05 15:45:22 +0200

    fix check

--- R/toolCheck.R
+++ R/toolCheck.R
@@ -3,7 +3,7 @@ toolCheck <- function(title, code) {
   n <- 100
   o <- c(paste(paste(rep("-", 3), collapse = ""), toupper(title),
                 paste(rep("-", n - nchar(title) - 5), collapse = "")),
-         sub("^~* ","", capture.output(out <- eval(code, envir = parent.frame()), type = "message")),
+         sub("^~* ","", utils::capture.output(out <- eval(code, envir = parent.frame()), type = "message")),
          paste(rep("-", n), collapse = ""))
   for(i in seq_along(o)) vcat(1, o[i], show_prefix = FALSE)
   options(toolCheck = c(getOption("toolCheck"), o))
