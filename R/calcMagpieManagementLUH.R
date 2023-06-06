7b6ad4bb8216d677725d177a8f36ac27747bfaf4
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-06-06 14:29:33 +0200

    wip

--- R/calcMagpieManagementLUH.R
+++ R/calcMagpieManagementLUH.R
@@ -1,11 +1,13 @@
 calcMagpieManagementLUH <- function() {
   # TODO flooded fraction of c3ann area: flood
-  floodedShare <- read.magpie(system.file("extdata/floodedShareRice.mz", package = "mrdownscale"))
+  flooded <- read.magpie(system.file("extdata/floodedShareRice.mz", package = "mrdownscale"))
+  floodedRaster <- magclass::as.SpatRaster(flooded)
+  target <- calcOutput("LandTargetData", aggregate = FALSE)
+  floodedTargetResolution <- terra::project(floodedRaster, target)
+
   rice <- readSource("Magpie")[, , "rice_pro"]
   commonYears <- intersect(getYears(shareFloodedRice, TRUE), getYears(rice, TRUE))
 
-  # ... to get historic data as weights, then disaggregate rice_pro
-  # - resolution = 67k or 59k cells
   # - project to target resolution, then disaggregate from cluster to target resolution
 
   # industrial roundwood fraction of wood harvest: rndwd
