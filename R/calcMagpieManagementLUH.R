9a98e2b6149203b55cbf66e3abab4e18d983ab6f
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-06-06 13:48:47 +0200

    prepFloodedShareRice

--- R/calcMagpieManagementLUH.R
+++ R/calcMagpieManagementLUH.R
@@ -1,6 +1,9 @@
 calcMagpieManagementLUH <- function() {
   # TODO flooded fraction of c3ann area: flood
-  riceHist <- read.magpie(system.file("riceHistorical.mz", package = "mrdownscale"))
+  floodedShare <- read.magpie(system.file("extdata/floodedShareRice.mz", package = "mrdownscale"))
+  rice <- readSource("Magpie")[, , "rice_pro"]
+  commonYears <- intersect(getYears(shareFloodedRice, TRUE), getYears(rice, TRUE))
+
   # ... to get historic data as weights, then disaggregate rice_pro
   # - resolution = 67k or 59k cells
   # - project to target resolution, then disaggregate from cluster to target resolution
