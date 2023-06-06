0cdf54edeb3545d64de95c8b9de7188b83c75dc6
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-06-06 13:48:47 +0200

    prepFloodedShareRice

--- data-raw/prepRiceHistorical.R
+++
@@ -1,6 +0,0 @@
-withr::with_package("mrcommons", {
-  riceHist <- calcOutput("Ricearea", cellular = TRUE,
-                         cells = "lpjcell", # = 67k cells, use magpiecell for 59k cells
-                         aggregate = FALSE, share = FALSE)
-  write.magpie(riceHist, "inst/extdata/riceHistorical.mz")
-})
