978bee11f1f38ba08380936abaa247b9f7bfe1d7
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-06-15 09:36:17 +0200

    formatting

--- R/calcLandHarmonized.R
+++ R/calcLandHarmonized.R
@@ -1,9 +1,9 @@
 calcLandHarmonized <- function(input = "magpie", target = "luh2",
                            harmonizeYear = 1995, finalYear = 2015, method = "fade") {
-  input  <- calcOutput("LandHarmonizedCategories", input = input, target = target, aggregate = FALSE)
+  input    <- calcOutput("LandHarmonizedCategories", input = input, target = target, aggregate = FALSE)
   geometry <- attr(input, "geometry")
   crs      <- attr(input, "crs")
-  target <- calcOutput("LandTargetData", target = target, aggregate = FALSE)
+  target   <- calcOutput("LandTargetData", target = target, aggregate = FALSE)
 
   # bring target data to spatial resolution of input data
   ref    <- as.SpatVector(input[, 1, 1])[, c(".region", ".id")]
@@ -19,11 +19,11 @@ calcLandHarmonized <- function(input = "magpie", target = "luh2",
     toolExpectLessDiff(inSum, inSum[, 1, ], 10^-5, "Total areas in input stay constant over time")
     toolExpectLessDiff(tSum, tSum[, 1, ], 10^-5, "Total areas in target stay constant over time")
     toolExpectLessDiff(inSum[, 1, ], tSum[, 1, ], 10^-5, "Total areas are the same in target and input data")
-    if(max(abs(inSum[, 1, ] - tSum[, 1, ])) >= 10^-5) {
+    if (max(abs(inSum[, 1, ] - tSum[, 1, ])) >= 10^-5) {
       corr <- setYears(dimSums(target[, 1, ], dim = 3) / dimSums(input[, 1, ], dim = 3), NULL)
       input <- input * corr
-      vcat(1, "[!] input data multiplied with correction factors to match target areas (max ratio = ", round(max(corr), 2),
-      ", min ratio = ", round(min(corr), 2),  ")", show_prefix = FALSE)
+      vcat(1, "[!] input data multiplied with correction factors to match target areas (max ratio = ",
+           round(max(corr), 2), ", min ratio = ", round(min(corr), 2),  ")", show_prefix = FALSE)
     }
   })
 
@@ -43,7 +43,7 @@ calcLandHarmonized <- function(input = "magpie", target = "luh2",
     toolExpectTrue(!is.null(attr(out, "crs")), "Data contains CRS information")
     toolExpectTrue(identical(unname(getSets(out)), c("region", "id", "year", "data")), "Dimensions are named correctly")
     toolExpectTrue(setequal(getItems(out, dim = 3), getItems(input, dim = 3)), "Land categories remain unchanged")
-    toolExpectTrue(all(out >= 0), "All values are > 0")
+    toolExpectTrue(all(out >= 0), "All values are >= 0")
     outSum <- dimSums(out, dim = 3)
     toolExpectLessDiff(outSum, outSum[, 1, ], 10^-5, "Total areas in output stay constant over time")
     toolExpectLessDiff(outSum, dimSums(input, dim = 3), 10^-5, "Total areas remain unchanged")
