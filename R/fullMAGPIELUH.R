630967317b9ff1d6cdd5204b4ea48a9adaabf89a
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-06-20 15:07:45 +0200

    prepend historic data

--- R/fullMAGPIELUH.R
+++ R/fullMAGPIELUH.R
@@ -2,26 +2,31 @@ fullMAGPIELUH <- function() {
   x <- calcOutput("MagpieStatesLUH", aggregate = FALSE)
 
   # TODO move this into calcMagpieStatesLUH when netcdf & SpatRasterDataset can be cached
+  history <- readSource("LUH2v2h", subset = FALSE, convert = FALSE)
+  history <- history[[terra::time(history) < 1995]]
+
   # fill years, write one .nc file for each category to prevent memory issues
   todisk <- terra::terraOptions(print = FALSE)$todisk
+  withr::defer({
+    terra::terraOptions(todisk = todisk)
+  })
   terra::terraOptions(todisk = TRUE)
   x <- as.SpatRaster(x)
   stopifnot(grepl("^y[0-9]{4}\\.\\.", names(x)))
   terra::time(x, tstep = "years") <- as.integer(substr(names(x), 2, 5))
   categories <- unique(sub("^y[0-9]{4}\\.\\.", "", names(x)))
   for (category in categories) {
-    message("filling years for ", category, "...")
+    message(category)
     filled <- toolFillYearsSpatRaster(x[category])
-    terra::writeCDF(filled, paste0(category, ".nc"), category, overwrite = TRUE)
+    extended <- terra::extend(filled, history)
+    combined <- c(history[category], extended)
+    terra::writeCDF(combined, paste0(category, ".nc"), category, overwrite = TRUE)
   }
 
   # combine into one single .nc file
   states <- terra::sds(paste0(categories, ".nc"))
   terra::writeCDF(states, "magpie_luh_states.nc")
-  unlink(paste0(categories, ".nc"))
-  terra::terraOptions(todisk = todisk)
-
-  # history <- readSource("LUH2v2h", subset = paste(850:1994, collapse = "|")) # TODO Error: ! std::bad_alloc
+  # unlink(paste0(categories, ".nc")) # TODO comment in
 
   management <- calcOutput("MagpieManagementLUH", aggregate = FALSE)
   stopifnot(grepl("^y[0-9]{4}\\.\\.", names(management)))
