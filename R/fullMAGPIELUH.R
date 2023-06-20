ddcc1ac7a5afaeb11246d6faadb38be83e4d62fe
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-06-20 10:31:59 +0200

    toolFillYearsSpatRaster

--- R/fullMAGPIELUH.R
+++ R/fullMAGPIELUH.R
@@ -1,24 +1,25 @@
 fullMAGPIELUH <- function() {
-  terra::terraOptions(memfrac = 0.2, todisk = TRUE)
   x <- calcOutput("MagpieStatesLUH", aggregate = FALSE)
-  # write.magpie(x, "magpie_luh_states.nc")
+
+  # TODO move this into calcMagpieStatesLUH when netcdf & SpatRasterDataset can be cached
+  # fill years, write one .nc file for each category to prevent memory issues
+  todisk <- terra::terraOptions(print = FALSE)$todisk
+  terra::terraOptions(todisk = TRUE)
   x <- as.SpatRaster(x)
-  terra::time(x, tstep = "years") <- as.integer(substr(names(x), 2, 5))
   stopifnot(grepl("^y[0-9]{4}\\.\\.", names(x)))
+  terra::time(x, tstep = "years") <- as.integer(substr(names(x), 2, 5))
   categories <- unique(sub("^y[0-9]{4}\\.\\.", "", names(x)))
-  lapply(categories, function(category) {
-    message("filling years for ", category, "... ", appendLF = FALSE)
-    filled <- fillYears(x[category])
-    terra::writeCDF(filled, paste0(category, ".nc"), category)
-    gc()
-    message("done.")
-    TRUE
-  })
+  for (category in categories) {
+    message("filling years for ", category, "...")
+    filled <- toolFillYearsSpatRaster(x[category])
+    terra::writeCDF(filled, paste0(category, ".nc"), category, overwrite = TRUE)
+  }
 
   # combine into one single .nc file
   states <- terra::sds(paste0(categories, ".nc"))
   terra::writeCDF(states, "magpie_luh_states.nc")
   unlink(paste0(categories, ".nc"))
+  terra::terraOptions(todisk = todisk)
 
   # history <- readSource("LUH2v2h", subset = paste(850:1994, collapse = "|")) # TODO Error: ! std::bad_alloc
 
@@ -29,35 +30,4 @@ fullMAGPIELUH <- function() {
   management <- terra::sds(datasets)
   names(management) <- varnames
   terra::writeCDF(management, "magpie_luh_management.nc")
-}
-
-# TODO integrate into toolFillYears or turn into separate tool function
-fillYears <- function(x, years = NULL) {
-  stopifnot(grepl("^y[0-9]{4}\\.\\.", names(x)[[1]]))
-  category <- sub("^y[0-9]{4}\\.\\.", "", names(x)[[1]])
-  xYears <- terra::time(x)
-  if (any(is.na(xYears))) {
-    stop("terra::time must be set for fillYears to work")
-  }
-  if (is.unsorted(xYears)) {
-    stop("xYears must be sorted")
-  }
-  years <- if (is.null(years)) min(xYears):max(xYears) else sort(years)
-  out <- lapply(years, function(year) {
-    if (year %in% xYears) {
-      return(x[as.character(year)])
-    }
-    # find the last xYear bigger than `year`
-    a <- Find(function(y) y < year, xYears, right = TRUE)
-    # find the first xYear bigger than `year`
-    b <- Find(function(y) y > year, xYears)
-    p <- (year - a) / (b - a)
-    stopifnot(p >= 0 && p <= 1)
-    # interpolate between anchor years a and b
-    out <- (1 - p) * x[as.character(a)] + p * x[as.character(b)]
-    stopifnot(terra::nlyr(out) == 1)
-    names(out) <- paste0("y", year, "..", category)
-    return(out)
-  })
-  return(do.call(c, out))
 }
