9a551a2f81c5f7768194ee9238d9af974347ed86
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-06-22 15:35:59 +0200

    minor things

--- R/toolCheckReport.R
+++ R/toolCheckReport.R
@@ -1,13 +1,10 @@
-toolCheckReport <- function(filter = FALSE, unlist = FALSE) {
+toolCheckReport <- function(filter = FALSE) {
   fname <- as.character(sys.call(-1))[1]
   x <- getOption("toolCheck")
   if (isTRUE(filter) && !is.na(fname)) {
     deps <- suppressWarnings(getDependencies(sub("^.*:::", "", fname),
                                              packages = getConfig("packages"), self = TRUE)$call)
     x <- x[names(x) %in% deps]
-  }
-  if (isTRUE(unlist)) {
-    x <- unlist(x, use.names = FALSE)
   }
   return(x)
 }
