e7636cc1a724314d61c739a6b3a57029f4e0e759
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-07-07 13:13:01 +0200

    nonland categories harmonized

---
+++ R/calcNonlandHarmonizedCategories.R
@@ -0,0 +1,42 @@
+calcNonlandHarmonizedCategories <- function(input = "magpie", target = "luh2") {
+  x <- toolAddCheckReport(calcOutput("NonlandInputData", input = input, aggregate = FALSE))
+
+  # separate categories that need to be mapped from those that don't
+  wood <- x[, , c("rndwd", "fulwd")]
+  x <- x[, , c("rndwd", "fulwd"), invert = TRUE]
+
+  # map categories using weights from land categorization
+  getItems(x, 3) <- sub("fertl_", "", getItems(x, 3))
+  map <- toolLandCategoriesMapping(input, target)
+
+  # get weights for disaggregation to reference categories
+  ref <- toolAddCheckReport(calcOutput("LandCategorizationWeight", map = map, geometry = attr(x, "geometry"),
+                                       crs = attr(x, "crs"), aggregate = FALSE))
+  map <- map[map$dataInput %in% getItems(x, 3), ]
+  ref <- ref[, , unique(map$merge)]
+  y   <- toolAggregate(x, map, dim = 3, from = "dataInput", to = "merge", weight = ref)
+  out <- toolAggregate(y, map, dim = 3, from = "merge",     to = "dataOutput")
+  out[, , "c3per"] <- out[, , "c3per"] + out[, , "c3per_biofuel_2nd_gen"]
+  out[, , "c4per"] <- out[, , "c4per"] + out[, , "c4per_biofuel_2nd_gen"]
+  out <- out[, , c("c3per_biofuel_2nd_gen", "c4per_biofuel_2nd_gen"), invert = TRUE]
+  getItems(out, 3) <- paste0("fertl_", getItems(out, 3))
+
+  out <- mbind(out, wood)
+  attr(out, "crs") <- attr(x, "crs")
+  attr(out, "geometry") <- attr(x, "geometry")
+
+  # check data for consistency
+  toolCheck("Nonland Harmonized Categories output", {
+    toolExpectTrue(identical(unname(getSets(out)), c("region", "id", "year", "data")), "Dimensions are named correctly")
+    expectedCategories <- c("fertl_c3ann", "fertl_c4ann", "fertl_c3per", "fertl_c4per", "fertl_c3nfx", "rndwd", "fulwd")
+    toolExpectTrue(setequal(getItems(out, dim = 3), expectedCategories), "Nonland categories match target definition")
+    toolExpectTrue(all(out >= 0), "All values are >= 0")
+  })
+
+  attr(out, "toolCheck") <- toolCheckReport(filter = TRUE)
+  return(list(x = out,
+              isocountries = FALSE,
+              unit = "rndwd, fulwd: 1; fertl_*: kg ha-1 yr-1", # TODO change fertl unit to kg yr-1
+              min = 0,
+              description = "Input data with nonland categories remapped to categories of target data set"))
+}
