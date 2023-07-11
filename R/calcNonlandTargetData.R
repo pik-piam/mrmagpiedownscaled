9adaed7095202ca2f434ae2645c56b55252c61de
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-07-11 14:20:59 +0200

    highres nonland works

--- R/calcNonlandTargetData.R
+++ R/calcNonlandTargetData.R
@@ -1,14 +1,21 @@
 calcNonlandTargetData <- function(target = "luh2") {
   if (target == "luh2") {
-    out <- readSource("LUH2v2h", subtype = "management", convert = FALSE)
-    out <- out["rndwd|fulwd|fertl"]
+    man <- readSource("LUH2v2h", subtype = "management", convert = FALSE)
+
+    # need absolute values for downscaling, fertl_* is in kg ha-1 yr-1, convert to kg yr-1
+    cellArea <- readSource("LUH2v2h", subtype = "cellArea", convert = FALSE)
+    fertl <- man["fertl"] * cellArea
+    terra::units(fertl) <- "kg yr-1"
+    names(fertl) <- paste0(sub("fertl_", "", names(fertl)), "_fertilizer")
+
+    out <- c(man["rndwd|fulwd"], fertl)
+    attr(out, "toolCheck") <- toolCheckReport(filter = TRUE)
+    return(list(x = out,
+                class = "SpatRaster",
+                cache = FALSE,
+                unit = "rndwd, fulwd: 1, fertl_*: kg yr-1",
+                description = "Nonland target data for data harmonization"))
   } else {
     stop("Unsupported output type \"", target, "\"")
   }
-  attr(out, "toolCheck") <- toolCheckReport(filter = TRUE)
-  return(list(x = out,
-              class = "SpatRaster",
-              cache = FALSE,
-              unit = "1",
-              description = "Nonland target data for data harmonization"))
 }
