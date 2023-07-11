867a39300f516d9a6350c97d37e25b5feee04ddd
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-07-11 14:20:59 +0200

    highres nonland works

--- R/calcNonlandInputData.R
+++ R/calcNonlandInputData.R
@@ -11,13 +11,14 @@ calcNonlandInputData <- function(input = "magpie") {
     wood[is.na(wood)] <- 0 # replace NAs introduced by division by zero
 
     fertilizer <- readSource("Magpie", subtype = "fertilizer")
-    cropArea <- readSource("Magpie", subtype = "land")[, , "crop"]
-    # convert from Tg Nr yr-1 to kg ha-1 yr-1
-    fertilizer <- fertilizer / collapseDim(cropArea) / 1e9
+    # convert from Tg Nr yr-1 to kg yr-1
+    fertilizer <- fertilizer / 10^9
     # clusters without crop area are NA, replace with 0
     fertilizer[is.na(fertilizer)] <- 0
-    fertilizer[fertilizer < 0] <- 0 # TODO there should not be negative values
-    getItems(fertilizer, 3) <- paste0("fertl_", getItems(fertilizer, 3))
+    # there are some negative values very close to zero, replace with 0
+    stopifnot(min(fertilizer) >= -10^-10)
+    fertilizer[fertilizer < 0] <- 0
+    getItems(fertilizer, 3) <- paste0(getItems(fertilizer, 3), "_fertilizer")
 
     out <- mbind(wood, fertilizer)
     attr(out, "geometry") <- geometry
@@ -37,7 +38,7 @@ calcNonlandInputData <- function(input = "magpie") {
   attr(out, "toolCheck") <- toolCheckReport(filter = TRUE)
   return(list(x = out,
               isocountries = FALSE,
-              unit = "rndwd, fulwd: 1, fertl_*: kg ha-1 yr-1",
+              unit = "rndwd, fulwd: 1, *_fertilizer: kg yr-1",
               min = 0,
               description = "Nonland input data for data harmonization and downscaling pipeline"))
 }
