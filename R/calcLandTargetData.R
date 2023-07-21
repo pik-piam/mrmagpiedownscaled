0481df9c265c5e99c217002e621a79d8ba0fed3a
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-07-21 14:25:31 +0200

    LandTargetData with irrigation

--- R/calcLandTargetData.R
+++ R/calcLandTargetData.R
@@ -6,42 +6,47 @@ calcLandTargetData <- function(target = "luh2") {
     states <- toolSpatRasterToDataset(states)
     man <- readSource("LUH2v2h", subtype = "management", convert = FALSE)
     man <- toolSpatRasterToDataset(man)
-    man <- man[paste0("crpbf_", cropTypes)]
+    man <- man[c(paste0("crpbf_", cropTypes), paste0("irrig_", cropTypes))]
     stopifnot(all.equal(terra::time(states[1]), terra::time(man[1])))
 
-    # crpbf_<cropType> = 1st gen biofuel crop share of <cropType>
-    # -> get <cropType>_biofuel_1st_gen area in Mha by multiplying with <cropType>
-    biofuel1stGen <- terra::sds(lapply(cropTypes, function(cropType) {
+    out <- list()
+    for (cropType in cropTypes) {
       crpbf <- man[paste0("crpbf_", cropType)]
-      stopifnot(all.equal(terra::time(crpbf), terra::time(states[cropType])))
-      out <- crpbf * states[cropType]
-      names(out) <- sub("\\.\\.crpbf_(.+)$", "..\\1_biofuel_1st_gen", names(out))
-      return(out)
-    }))
-    names(biofuel1stGen) <- paste0(cropTypes, "_biofuel_1st_gen")
+      irrig <- man[paste0("irrig_", cropType)]
+      stopifnot(all.equal(terra::time(crpbf), terra::time(states[cropType])),
+                all.equal(terra::time(irrig), terra::time(states[cropType])))
 
-    # 2nd gen biofuel is not part of LUH2v2h, but we need it for the harmonization, so fill with zeros
-    perennials <- c("c3per", "c4per")
-    biofuel2ndGen <- terra::sds(lapply(perennials, function(cropType) {
-      out <- states[cropType] * 0
-      names(out) <- paste0(names(out), "_biofuel_2nd_gen")
-      return(out)
-    }))
-    names(biofuel2ndGen) <- paste0(perennials, "_biofuel_2nd_gen")
+      # crpbf_<cropType> = 1st gen biofuel crop share of <cropType>
+      # -> get <cropType>_biofuel_1st_gen area in Mha by multiplying with <cropType>
+      biofuel1stGen <- crpbf * states[cropType]
 
-    crops <- terra::sds(lapply(cropTypes, function(cropType) {
-      biofuel1 <- biofuel1stGen[paste0(cropType, "_biofuel_1st_gen")]
-      biofuel2 <- if (cropType %in% perennials) biofuel2ndGen[paste0(cropType, "_biofuel_2nd_gen")] else 0
-      return(states[cropType] - biofuel1 - biofuel2)
-    }))
-    names(crops) <- cropTypes
+      irrigatedBiofuel1stGen <- irrig * biofuel1stGen
+      rainfedBiofuel1stGen <- biofuel1stGen - irrigatedBiofuel1stGen
+      names(irrigatedBiofuel1stGen) <- sub("\\.\\..+$", paste0("..", cropType, "_irrigated_biofuel_1st_gen"),
+                                           names(irrigatedBiofuel1stGen))
+      names(rainfedBiofuel1stGen) <- sub("\\.\\..+$", paste0("..", cropType, "_rainfed_biofuel_1st_gen"),
+                                         names(rainfedBiofuel1stGen))
 
-    states <- c(states[setdiff(names(states), cropTypes)], crops)
-    man <- c(man[setdiff(names(man), paste0("crpbf_", cropTypes))],
-             biofuel1stGen,
-             biofuel2ndGen)
+      nonBiofuel <- states[cropType] - biofuel1stGen
 
-    out <- do.call(c, as.list(c(states, man)))
+      rainfedBiofuel2ndGen <- NULL
+      if (grepl("per", cropType)) {
+        # 2nd gen biofuel is not part of LUH2v2h, but we need it for the harmonization, so fill with zeros
+        rainfedBiofuel2ndGen <- 0 * states[cropType]
+        names(rainfedBiofuel2ndGen) <- paste0(names(rainfedBiofuel2ndGen), "_rainfed_biofuel_2nd_gen")
+        # we assume 2nd gen biofuel is never irrigated, so irrigatedBiofuel2ndGen does not exist
+        nonBiofuel <- nonBiofuel - rainfedBiofuel2ndGen
+      }
+
+      irrigatedNonBiofuel <- irrig * nonBiofuel
+      rainfedNonBiofuel <- nonBiofuel - irrigatedNonBiofuel
+      names(irrigatedNonBiofuel) <- sub("\\.\\..+$", paste0("..", cropType, "_irrigated"), names(irrigatedNonBiofuel))
+      names(rainfedNonBiofuel) <- sub("\\.\\..+$", paste0("..", cropType, "_rainfed"), names(rainfedNonBiofuel))
+
+      out <- c(out, irrigatedNonBiofuel, rainfedNonBiofuel,
+               irrigatedBiofuel1stGen, rainfedBiofuel1stGen, rainfedBiofuel2ndGen)
+    }
+    out <- do.call(c, out)
   } else {
     stop("Unsupported output type \"", target, "\"")
   }
