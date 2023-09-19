decf1eef9b0dc0b672e41ce8a84c9543a84b94ad
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-09-19 11:50:02 +0200

    fertilizer downscaling & reporting

--- R/calcNonlandHighRes.R
+++ R/calcNonlandHighRes.R
@@ -1,31 +1,50 @@
 #' calcNonlandHighRes
 #'
 #' This function performs the downscaling: It calculates a high resolution dataset
-#' from the low resolution input dataset and the high resolution target dataset
-#' using the given downscaling method.
+#' from the low resolution input dataset and the high resolution target dataset.
 #'
 #' @param input name of an input dataset, currently only "magpie"
 #' @param target name of a target dataset, currently only "luh2"
-#' @param downscaling name of downscaling method, currently only "magpieClassic"
 #' @return downscaled nonland data
-#' @author Jan Philipp Dietrich
-calcNonlandHighRes <- function(input = "magpie", target = "luh2", downscaling = "magpieClassic") {
-  x <- calcOutput("NonlandHarmonized", input = input, target = target, aggregate = FALSE)
+#' @author Pascal Sauer
+calcNonlandHighRes <- function(input = "magpie", target = "luh2") {
+  xInput <- calcOutput("NonlandHarmonized", input = input, target = target, aggregate = FALSE)
   xTarget <- calcOutput("NonlandTargetData", target = target, aggregate = FALSE)
 
-  if (downscaling == "magpieClassic") {
-    # suppressing warning: Total stock is not constant over time.
-    # This is not a problem because we're not downscaling land use data.
-    suppressWarnings({
-      out <- toolDownscaleMagpieClassic(x, xTarget)
-    })
-  } else {
-    stop("Unsupported downscaling method \"", downscaling, "\"")
-  }
+  # simple weighted disaggregation for fertl
+  cropTypes <- c("c3ann", "c3nfx", "c3per", "c4ann", "c4per")
+  downscaledFertilizer <- lapply(cropTypes, function(cropType) {
+    fertilizer <- as.SpatVector(xInput[, , grep(paste0(cropType, "_fertilizer"), getNames(xInput))])
+    fertilizer$.region <- NULL
+    fertilizer$.id <- NULL
+
+    # use latest year of historical data as weight
+    weight <- xTarget[[terra::time(xTarget) == max(terra::time(xTarget))]]
+    weight <- weight[paste0(cropType, "_fertilizer")]
+    names(weight) <- sub("^y[0-9]+\\.\\.", "", names(weight))
+
+    # calculate weight sum in each cluster
+    x <- terra::extract(weight, fertilizer, bind = TRUE, fun = "sum", na.rm = TRUE)
+
+    # divide input cluster values by weight sum, now it only needs to be multiplied by weight and we're done
+    for (n in names(fertilizer)) {
+      x[[n]] <- x[[n]] / x[[paste0(cropType, "_fertilizer")]]
+    }
+    x[[paste0(cropType, "_fertilizer")]] <- NULL
+
+    factorRaster <- do.call(c, lapply(names(x), function(name) terra::rasterize(x, weight, name)))
+    result <- factorRaster * weight
+    terra::time(result, tstep = "years") <- as.integer(sub("^y([0-9]+)\\.\\..+$", "\\1", names(result)))
+    return(result)
+  })
+  downscaledFertilizer <- do.call(c, downscaledFertilizer)
+  out <- as.magpie(downscaledFertilizer)
+
+  # TODO rndwd and fulwd
 
   return(list(x = out,
               isocountries = FALSE,
-              unit = "rndwd, fulwd: 1, *_fertilizer: kg yr-1",
+              unit = "*_fertilizer: kg yr-1",
               min = 0,
               description = "Downscaled nonland data"))
 }
---
+++ R/calcNonlandReport.R
@@ -0,0 +1,29 @@
+#' calcNonlandReport
+#'
+#' Convert the downscaled nonland data to the format required by the given project.
+#'
+#' @param project name of the project, currently only "RESCUE"
+#' @return nonland data
+#' @author Pascal Führlich
+calcNonlandReport <- function(project = "RESCUE") {
+  if (project == "RESCUE") {
+    x <- calcOutput("NonlandHighRes", input = "magpie", target = "luh2", aggregate = FALSE)
+
+    # *_fertilizer is in kg yr-1, need to report in kg ha-1 yr-1, so divide by cell area
+    cellArea <- readSource("LUH2v2h", subtype = "cellArea", convert = FALSE)
+    cellArea <- collapseDim(as.magpie(cellArea), 3)
+    stopifnot(getItems(x, 1) %in% getItems(cellArea, 1))
+    x <- x / (cellArea[getItems(x, 1), , ] * 100) # divide by cell area in ha (*100 to convert from km2 to ha)
+    getNames(x) <- sub("^(.+)_fertilizer$", "fertl_\\1", getNames(x))
+
+    # TODO report fulwd & rndwd
+
+    return(list(x = x,
+                isocountries = FALSE,
+                unit = "rndwd, fulwd: 1, fertl_*: kg ha-1 yr-1",
+                min = 0,
+                description = "Downscaled nonland data report for RESCUE"))
+  } else {
+    stop("Can only report for project = 'RESCUE'")
+  }
+}
