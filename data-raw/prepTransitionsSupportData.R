58b0453cda2c10c0860fd2e06f139850a2585023
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2023-10-14 12:07:19 +0200

    some tweaks

--- data-raw/prepTransitionsSupportData.R
+++ data-raw/prepTransitionsSupportData.R
@@ -1,8 +1,9 @@
 extractTransitions <- function(file = "transitions.nc", years=1:3) {
   # extract years from transitions.nc and return them in magpie format
   a <- rast("../../mrdownscale_data/LUH2v2/transitions.nc")
-  b <- subset(a, grep(paste0("_(",paste(years, collapse="|"), ")$"), names(a)))
-  names(b) <- sub("^(.*)_(.*)$", "\\2..\\1",names(b))
+  b <- subset(a, grep(paste0("_to_.*_(",paste(years, collapse="|"), ")$"), names(a)))
+  names(b) <- sub("^(.*)_to_(.*)_(.*)$", "\\3..\\1.\\2",names(b))
   m <- as.magpie(b)
+  getSets(m, fulldim=FALSE)[3] <- "from.to"
   return(m)
 }
