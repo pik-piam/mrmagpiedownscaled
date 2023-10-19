fe9ce473bebe4ce917aa93e8872a86bdcfebdd20
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2023-10-19 15:21:36 +0200

    refined transistions preparation script

--- data-raw/prepTransitionsSupportData.R
+++ data-raw/prepTransitionsSupportData.R
@@ -74,30 +74,57 @@ getBidirectionalTransistions <- function(b) {
   return(bmin)
 }
 
+
+ncFolder <- "../../mrdownscale_data/LUH2v2/"
+computationFolder <- "../../mrdownscale_data/"
+
+# 20 year time horizon 1995 - 2016
+# 4 year packages (to stay in memory limits)
 range <- seq(1995, 2011, 4)
 tSteps <- 4
 
+# extract transition information in digestable blocks
 for (i in range) {
-  file <- paste0("../../mrdownscale_data/transitions_", i, "to", i + tSteps - 1, ".mz")
-  write.magpie(extractTransitions("../../mrdownscale_data/LUH2v2/transitions.nc", years = (i:(i + 3))),
+  file <- file.path(computationFolder, paste0("transitions_", i, "to", i + tSteps - 1, ".mz"))
+  write.magpie(extractTransitions(file.path(ncFolder, "transitions.nc"), years = (i:(i + 3))),
                file)
   cat(basename(file), "written.")
 }
 
+# read in blocks and generate single 20 year block
 out <- NULL
 for (i in range) {
-  file <- paste0("../../mrdownscale_data/transitions_", i, "to", i + tSteps - 1, ".mz")
+  file <- file.path(computationFolder, paste0("transitions_", i, "to", i + tSteps - 1, ".mz"))
   m <- read.magpie(file)
   m[m < 0] <- 0 # fix data errors (negative values due to rounding issues)
   out <- mbind(out, m)
 }
-write.magpie(out, "../../mrdownscale_data/transitions_1995to2015.mz")
+write.magpie(out, file.path(computationFolder, "transitions_1995to2015.mz"))
 
-a <- read.magpie("../../mrdownscale_data/transitions_1995to2015.mz")
+# compute bidirectional transitions (transitions which happen in a cell in the
+# same size in both directions or in other words: transitions, which don't lead
+# to net transitions)
+a <- read.magpie(file.path(computationFolder, "transitions_1995to2015.mz"))
 abi <- getBidirectionalTransistions(a)
-write.magpie(abi, "../../mrdownscale_data/bidirectionalTransistions_1995to2015.mz")
+write.magpie(abi, file.path(computationFolder, "bidirectionalTransistions_1995to2015.mz"))
+# compute mean bilateral transitions over the selected time period
 meanBi <- dimSums(abi, dim = 2) / dim(abi)[2]
-write.magpie(meanBi, "../../mrdownscale_data/meanBidirectionalTransistions_1995to2015.mz")
+write.magpie(meanBi, file.path(computationFolder, "meanBidirectionalTransistions_1995to2015.mz"))
 
-o <- extractStatesMean("../../mrdownscale_data/LUH2v2/states.nc")
-write.magpie(o, "../../mrdownscale_data/meanStates_1995to2016.mz")
+# calculate corresponding mean state levels for the corresponding period
+states <- extractStatesMean(file.path(ncFolder, "states.nc"), years = 1995:2016)
+write.magpie(states, file.path(computationFolder, "meanStates_1995to2016.mz"))
+
+# for every possible transition between two states select the smaller state area
+# of the two as reference area for the corresponding transition
+smallerArea <- mrdownscale:::toolGetSmallerArea(states)
+smallerArea <- smallerArea[,,getItems(meanBi, dim = 3)]
+
+# remove very small values
+meanBi[meanBi < 10^-6] <- 0
+
+# compute transformation share relative to the smaller area to have a
+# area-scalable representative value of the bidirectional transistions
+x <- meanBi/(smallerArea+10^-10)
+
+
