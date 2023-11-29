6fd63c4a563d6db9438bfa5d0390a935cc798360
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2023-11-29 17:35:39 +0100

    added cluster start scripts

---
+++ data-raw/startRun.R
@@ -0,0 +1,4 @@
+library(mrdownscale)
+setConfig(mainfolder="/p/projects/rd3mod/tmp/mrdownscale")
+retrieveData("RESCUE")
+
---
+++ data-raw/submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --qos=priority
+#SBATCH --job-name=mrdownscale
+#SBATCH --output=slurm-%j.log
+#SBATCH --mail-type=END
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=0
+#SBATCH --partition=priority
+
+Rscript startRun.R
