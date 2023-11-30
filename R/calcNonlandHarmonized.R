af31c534a228a3a137bb01fc7336ee6ef7c0fc5e
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-11-30 14:37:51 +0100

    NonlandHarmonized

--- R/calcNonlandHarmonized.R
+++ R/calcNonlandHarmonized.R
@@ -15,51 +15,25 @@
 calcNonlandHarmonized <- function(input = "magpie", target = "luh2mod",
                                   harmonizeYear = 2015, finalYear = 2050,
                                   method = "extrapolateFade") {
-  input <- calcOutput("NonlandHarmonizedCategories", input = input, aggregate = FALSE)
-  geometry <- attr(input, "geometry")
-  crs <- attr(input, "crs")
+  xInput <- calcOutput("NonlandHarmonizedCategories", input = input, aggregate = FALSE)
+  geometry <- attr(xInput, "geometry")
+  crs <- attr(xInput, "crs")
 
-  target <- calcOutput("NonlandTarget", target = target, aggregate = FALSE)
+  xTarget <- calcOutput("NonlandTarget", target = target, aggregate = FALSE)
   # bring target data to spatial resolution of input data
-  ref <- as.SpatVector(input[, 1, 1])[, c(".region", ".id")]
-  shareCategories <- c("rndwd", "fulwd")
-  nonShareCategories <- "fertilizer" # TODO explicitly list all non-share categories so line 46 can be simplified
-  # stopifnot(grepl(paste(c(shareCategories, nonShareCategories), collapse = "|"), names(target)))
-  targetNonShare <- terra::extract(target[nonShareCategories], ref, sum, na.rm = TRUE, bind = TRUE)
+  ref <- as.SpatVector(xInput[, 1, 1])[, c(".region", ".id")]
+  xTarget <- terra::extract(xTarget, ref, sum, na.rm = TRUE, bind = TRUE)
+  xTarget <- as.magpie(xTarget)
 
-  # spatially distribute wood harvest according to primf + secdf
-  land <- readSource("LUH2v2h")
-  stopifnot(terra::units(land) == "Mha")
-  forest <- land["primf"] + land["secdf"]
-  forestWoodHarvest <- terra::extract(target[paste(shareCategories, collapse = "|")] * forest,
-                                      ref, sum, na.rm = TRUE, bind = TRUE)
-  forestWoodHarvest <- as.magpie(forestWoodHarvest)
-  forestCluster <- terra::extract(forest, ref, sum, na.rm = TRUE, bind = TRUE)
-  woodHarvest <- forestWoodHarvest / collapseDim(as.magpie(forestCluster))
-  woodHarvest[forestWoodHarvest == 0] <- 0 # TODO: avoid NaNs?
-
-  # use weighted average, weight = primf + secdf area
-  target <- mbind(woodHarvest, as.magpie(targetNonShare))
-  # stopifnot(setequal(getItems(input, 3), getItems(target, 3)))
-  target <- target[, , getItems(input, 3)] # harmonize order of dim 3
+  stopifnot(setequal(getItems(xInput, 3), getItems(xTarget, 3)))
+  xTarget <- xTarget[, , getItems(xInput, 3)] # harmonize order of dim 3
 
   if (method == "extrapolateFade") {
-    nonShareIndices <- grep(paste(nonShareCategories, collapse = "|"), getItems(input, 3), value = TRUE)
-    nonSharesHarmonized <- toolHarmonizeExtrapolateFade(input[, , nonShareIndices],
-                                                        target[, , nonShareIndices],
-                                                        harmonizeYear = harmonizeYear,
-                                                        finalYear = finalYear,
-                                                        constantSum = FALSE)
-    # shareIndices <- grep(paste(shareCategories, collapse = "|"), getItems(input, 3), value = TRUE)
-    # sharesHarmonized <- toolHarmonizeExtrapolateFade(input[, , shareIndices],
-    #                                                  target[, , shareIndices],
-    #                                                  harmonizeYear = harmonizeYear,
-    #                                                  finalYear = finalYear,
-    #                                                  constantSum = TRUE)
-    out <- mbind(nonSharesHarmonized)#, sharesHarmonized)
+    out <- toolHarmonizeExtrapolateFade(xInput, xTarget, harmonizeYear = harmonizeYear,
+                                        finalYear = finalYear, constantSum = FALSE)
   } else {
     harmonizer <- toolGetHarmonizer(method)
-    out <- harmonizer(input, target, harmonizeYear = harmonizeYear, finalYear = finalYear)
+    out <- harmonizer(xInput, xTarget, harmonizeYear = harmonizeYear, finalYear = finalYear)
   }
 
   attr(out, "geometry") <- geometry
@@ -69,14 +43,13 @@ calcNonlandHarmonized <- function(input = "magpie", target = "luh2mod",
   toolExpectTrue(!is.null(attr(out, "geometry")), "Data contains geometry information")
   toolExpectTrue(!is.null(attr(out, "crs")), "Data contains CRS information")
   toolExpectTrue(identical(unname(getSets(out)), c("region", "id", "year", "data")), "Dimensions are named correctly")
-  # toolExpectTrue(setequal(getItems(out, dim = 3), getItems(input, dim = 3)), "Nonland categories remain unchanged")
+  toolExpectTrue(setequal(getItems(out, dim = 3), getItems(xTarget, dim = 3)), "Nonland categories remain unchanged")
   toolExpectTrue(all(out >= 0), "All values are >= 0")
-  # toolExpectTrue(all(out[, , shareCategories] <= 1.0001), "All shares are <= 1")
 
   return(list(x = out,
               class = "magpie",
               isocountries = FALSE,
-              unit = "rndwd, fulwd: 1; *_fertilizer: kg yr-1",
+              unit = "harvest_weight & bioh: kg C yr-1; harvest_area: Mha; fertilizer: kg yr-1",
               min = 0,
               description = "Harmonized nonland data"))
 }
