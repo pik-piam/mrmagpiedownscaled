ff50bccffe724a14ed74ea12d138cb06b9ae9d99
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-12-07 14:20:28 +0100

    calcLandTransitions less memory hungry combining

--- R/calcLandTransitions.R
+++ R/calcLandTransitions.R
@@ -11,8 +11,7 @@
 #' gross transition estimate based on average gross transitions in LUH2 in the
 #' period from 1995 to 2015 will be used.
 #' @return land use transition data
-#' @author Jan Philipp Dietrich
-
+#' @author Jan Philipp Dietrich, Pascal Sauer
 calcLandTransitions <- function(project = "RESCUE", gross = TRUE) {
   if (project != "RESCUE") stop("Can only report for project = 'RESCUE'")
 
@@ -21,15 +20,27 @@ calcLandTransitions <- function(project = "RESCUE", gross = TRUE) {
 
   l <- 2
   sequence <- seq(1, nyears(land) - 1, l)
+  tempfolder <- withr::local_tempdir()
   for (i in sequence) {
     message("Compute ", getYears(land)[i], " to ", getYears(land)[min(nyears(land), i + l)])
-    write.magpie(toolTransitionsBasic(land[, i:min(nyears(land), i + l), ], gross = gross), paste0(i, ".mz"))
+    write.magpie(toolTransitionsBasic(land[, i:min(nyears(land), i + l), ], gross = gross),
+                 paste0(tempfolder, "/", i, ".mz"))
   }
-  out <- lapply(paste0(sequence, ".mz"), read.magpie)
-  withr::with_options(list(magclass_setMatching = TRUE, magclass_sizeLimit = 10^11), {
-    out <- mbind(out)
-  })
-  unlink(paste0(sequence, ".mz"))
+
+  # allocate enough memory for everything instead of mbinding to avoid memory problems
+  message("allocating memory...")
+  coords <- getItems(land, dim = 1)
+  years <- utils::head(getYears(land, as.integer = TRUE), -1) + 1
+  variables <- getItems(read.magpie(paste0(tempfolder, "/", sequence[1], ".mz")), dim = 3)
+  out <- new.magpie(coords, years, variables, fill = NA_real_)
+
+  for (j in seq_along(sequence)) {
+    i <- sequence[j]
+    message(j, "/", length(sequence), " combining...")
+    y <- years[i:min(length(years), i + l - 1)]
+    out[, y, ] <- read.magpie(paste0(tempfolder, "/", i, ".mz"))
+  }
+  message("combining done, returning")
 
   return(list(x = out,
               isocountries = FALSE,
