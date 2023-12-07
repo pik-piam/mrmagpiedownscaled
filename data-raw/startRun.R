ec9f0cce45259dbdfec0b069142ae26824b07c91
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-12-07 16:10:53 +0100

    build

--- data-raw/startRun.R
+++ data-raw/startRun.R
@@ -1,3 +1,28 @@
 library(mrdownscale) # nolint
 setConfig(mainfolder = "/p/projects/rd3mod/tmp/mrdownscale") # nolint
-retrieveData("RESCUE", rev = format(Sys.time(), "%Y-%m-%d"))
+
+basePath <- "/p/projects/rescue/tier1_scenarios/v3/cpl/magpie/output/C_" # nolint
+scenarios <- c("RESCUE-dir-v2p0-EocBudg1150-OAE_off",
+               "RESCUE-dir-v2p0-EocBudg500-OAE_off",
+               "RESCUE-dir-v2p0-PkBudg1150-OAE_off",
+               "RESCUE-dir-v2p0-PkBudg1150-OAE_on",
+               "RESCUE-dir-v2p0-PkBudg500-OAE_off",
+               "RESCUE-dir-v2p0-PkBudg500-OAE_on",
+               "SSP2EU-Base",
+               "SSP2EU-NDC",
+               "SSP2EU-NPi")
+
+for (scenario in scenarios) {
+  stopifnot(file.exists(paste0(basePath, scenario, "-mag-7/",
+                               c("fulldata.gdx", "clustermap_rev4.94_c200_67420_h12.rds"))))
+}
+
+now <- format(Sys.time(), "%Y-%m-%d")
+
+for (scenario in scenarios) {
+  file.copy(paste0(basePath, scenario, "-mag-7/",
+                   c("fulldata.gdx", "clustermap_rev4.94_c200_67420_h12.rds")),
+            file.path(getConfig("sourcefolder"), "Magpie"),
+            overwrite = TRUE)
+  retrieveData("RESCUE", rev = paste0(gsub("_", "-", scenario), "-", now))
+}
