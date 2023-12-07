0bcfa1430ff5c1082827bfc44d96b9cd3b8cf50e
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2023-12-07 16:57:57 +0100

    cannot use scenario as rev

--- .Rprofile
+++ .Rprofile
@@ -1,3 +1,4 @@
+Sys.setenv(RENV_PATHS_LIBRARY = "renv/library")
 source("renv/activate.R")
 if (file.exists("Rprofile")) {
   source("Rprofile")
--- R/fullRESCUE.R
+++ R/fullRESCUE.R
@@ -5,18 +5,19 @@
 #'
 #' @param rev revision number of the data. If not provided the current date will be used instead
 #' @param ... reserved for future use
+#' @param scenario scenario name to be included in filenames
 #' @param compression compression level of the resulting .nc files, possible values are integers from 1-9,
 #' 1 = fastest, 9 = best compression
 #' @param interpolate boolean defining whether the data should be interpolated to annual values or not
 #'
 #' @author Pascal Sauer, Jan Philipp Dietrich
-fullRESCUE <- function(rev = NULL, ..., compression = 2, interpolate = FALSE) {
+fullRESCUE <- function(rev = NULL, ..., scenario = "", compression = 2, interpolate = FALSE) {
   stopifnot(...length() == 0)
 
   now <- Sys.time()
   version <- if (is.null(rev)) format(now, "%Y-%m-%d") else rev
   fileSuffix <- paste0("_input4MIPs_landState_RESCUE_PIK-MAgPIE-4-7-",
-                       version, "_gn_1995-2100")
+                       scenario, if (scenario == "") "" else "-", version, "_gn_1995-2100")
 
   land <- calcOutput("LandReport", project = "RESCUE", aggregate = FALSE)
   nonland <- calcOutput("NonlandReport", project = "RESCUE",
