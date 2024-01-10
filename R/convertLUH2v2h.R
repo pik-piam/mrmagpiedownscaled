9cf1b81f6ab99bda9faf37e5aab2dd03d0fc04c6
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2024-01-10 10:36:32 +0100

    fix LUH wood harvest area conversion to Mha

--- R/convertLUH2v2h.R
+++ R/convertLUH2v2h.R
@@ -3,7 +3,7 @@
 #' Convert LUH2 cell area shares to absolute areas by multiplying with cell area, convert to Mha.
 #'
 #' @param x SpatRaster with LUH2 cell area shares
-#' @param subtype Only "states" and "woodHarvestArea" are converted
+#' @param subtype Only "states" is converted
 convertLUH2v2h <- function(x, subtype = "states") {
   cellArea <- terra::rast("staticData_quarterdeg.nc", "carea")
   stopifnot(terra::units(cellArea) == "km2")
@@ -16,20 +16,8 @@ convertLUH2v2h <- function(x, subtype = "states") {
     x <- x * cellAreaMha
     unit <- "Mha"
     terra::units(x) <- unit
-  } else if (subtype == "transitions") {
-    harv <- x["harv"]
-    harv <- harv * cellAreaMha
-    names(harv) <- sub("harv", "wood_harvest_area", names(harv))
-    terra::units(harv) <- "Mha yr-1"
-
-    bioh <- x["bioh"]
-    bioh <- bioh * 1 # force bioh into memory, otherwise the result cannot be cached
-    terra::units(bioh) <- "kg C yr-1"
-
-    x <- c(harv, bioh)
-    unit <- "*_bioh: kg C yr-1, *_harv: Mha yr-1"
   } else {
-    stop("subtype must be states or transitions, for management and cellArea pass convert = FALSE")
+    stop("subtype must be states, for other subtypes pass convert = FALSE")
   }
 
   return(list(x = x,
