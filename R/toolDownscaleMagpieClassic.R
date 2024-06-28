31a233fdbb5ca3114716a922906ff8e0ad21a591
Pascal Sauer <pascal.sauer@pik-potsdam.de>
2024-06-28 16:12:51 +0200

    wip use resmap for downscaling

--- R/toolDownscaleMagpieClassic.R
+++ R/toolDownscaleMagpieClassic.R
@@ -6,41 +6,20 @@
 #' @param x magpie dataset containing land to be downscaled
 #' @param xTarget target land use dataset as SpatRaster for initialization year
 #' @return downscaled land use dataset
-#' @author Jan Philipp Dietrich
+#' @author Jan Philipp Dietrich, Pascal Sauer
 toolDownscaleMagpieClassic <- function(x, xTarget) {
-
-  .getDownscaleMap <- function(x, xTarget) {
-    tmp <- as.SpatVector(x[, 1, 1])[, ".id"]
-    tmp[[1]]$.id <- as.integer(tmp[[1]]$.id)
-    tmp <- terra::rasterize(tmp, xTarget, ".id", touches = FALSE)
-    names(tmp) <- "id"
-    tmp <- as.magpie(tmp)
-    map <- data.frame(cell = getItems(tmp, dim = 1), cluster = as.vector(tmp))
-
-    clusternames <- getItems(x, dim = 1)
-    names(clusternames) <- getItems(x, dim = 1.1)
-    map$cluster <- clusternames[map$cluster]
-
-    return(map)
-  }
-
-  mapping_old <- .getDownscaleMap(x, xTarget) # TODO remove old mapping and .getDownscaleMap
   mapping <- calcOutput("ResolutionMapping", aggregate = FALSE)
-  mapping$cell <- paste0(sub("\\.", "p", mapping$x), ".", sub("\\.", "p", mapping$y))
   mapping$cluster <- mapping$lowRes
   mapping <- mapping[, c("cell", "cluster")]
 
-  mTarget <- as.magpie(xTarget)
-
-  rm(xTarget)
-  gc()
+  mTarget <- as.magpie(xTarget[[terra::time(xTarget) == terra::time(xTarget)[1]]])
 
   "!# @monitor luscale::interpolate2"
 
   # interpolate2 assumes constant total over time, but only warns if unfulfilled, convert that to error
-  withr::with_options(c(warn = 2), {
-    out <- luscale::interpolate2(x[, -1, ], mTarget[, 1, ], mapping)
-  })
+  tryCatch({
+    out <- luscale::interpolate2(x[, -1, ], mTarget, mapping)
+  }, warning = stop)
   getSets(out)[1:2] <- c("x", "y")
 
   return(out)
