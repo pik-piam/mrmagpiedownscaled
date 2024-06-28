ff8c627d5221be3f7a41d5503a0da3ec57029ff9
Pascal Sauer <pascal.sauer@pik-potsdam.de>
2024-06-28 14:55:10 +0200

    add cell and cellOriginal to resmap

--- R/calcResolutionMapping.R
+++ R/calcResolutionMapping.R
@@ -19,14 +19,19 @@ calcResolutionMapping <- function(input = "magpie", target = "luh2mod") {
     coords <- strsplit(mapping$cell, "\\.")
     xCoords <- vapply(coords, function(x) as.double(sub("p", ".", x[1])), double(1))
     yCoords <- vapply(coords, function(x) as.double(sub("p", ".", x[2])), double(1))
+    mapping$cellOriginal <- sub("\\.[A-Z]{3}$", "", mapping$cell)
     mapping <- cbind(x = xCoords, y = yCoords, mapping[, -which(colnames(mapping) == "cell")])
     colnames(mapping)[colnames(mapping) == "cluster"] <- "lowRes"
   } else {
     stop("Unsupported input type \"", input, "\"")
   }
 
-  xTarget <- calcOutput("LandTarget", target = target, aggregate = FALSE)
-  mapping <- toolResolutionMapping(mapping, xTarget)
+  if (target == "luh2mod") {
+    targetGrid <- readSource("LUH2v2h", subtype = "states")
+  } else {
+    stop("Unsupported target type \"", target, "\"")
+  }
+  mapping <- toolResolutionMapping(mapping, targetGrid)
   return(list(x = mapping,
               class = "data.frame",
               unit = NA,
@@ -40,30 +45,30 @@ calcResolutionMapping <- function(input = "magpie", target = "luh2mod") {
 #' assuming target resolution is finer than what mapping already provides.
 #'
 #' @param mapping a data.frame with columns x, y, lowRes
-#' @param xTarget a terra SpatRaster with the target resolution
+#' @param targetGrid a terra SpatRaster with the target resolution
 #' @return a data.frame with columns x, y, lowRes, countrycode
 #'
 #' @author Pascal Sauer
 #' @export
-toolResolutionMapping <- function(mapping, xTarget) {
+toolResolutionMapping <- function(mapping, targetGrid) {
   stopifnot(c("x", "y", "lowRes") %in% colnames(mapping))
+  mappingColumns <- colnames(mapping)
   mapping$cellId <- seq_len(nrow(mapping))
-  pointsMapping <- terra::vect(mapping, geom = c("x", "y"), crs = terra::crs(xTarget))
+  pointsMapping <- terra::vect(mapping, geom = c("x", "y"), crs = terra::crs(targetGrid))
   mappingRes <- guessResolution(mapping[, c("x", "y")])
-  xTargetRes <- terra::res(xTarget)
-  stopifnot(xTargetRes[1] == xTargetRes[2],
-            xTargetRes[1] < mappingRes,
-            mappingRes %% xTargetRes[1] == 0)
-  xTargetAggregated <- terra::aggregate(xTarget, fact = mappingRes / xTargetRes)
-  rasterMapping <- terra::rasterize(pointsMapping, xTargetAggregated, field = "cellId")
+  targetGridRes <- terra::res(targetGrid)
+  stopifnot(targetGridRes[1] == targetGridRes[2],
+            targetGridRes[1] < mappingRes,
+            mappingRes %% targetGridRes[1] == 0)
+  targetGridAggregated <- terra::aggregate(targetGrid, fact = mappingRes / targetGridRes)
+  rasterMapping <- terra::rasterize(pointsMapping, targetGridAggregated, field = "cellId")
   names(rasterMapping) <- "cellId"
   polygonsMapping <- terra::as.polygons(rasterMapping)
-  rasterMapping <- terra::rasterize(polygonsMapping, xTarget, field = "cellId")
-  mappingColumns <- colnames(mapping)
+  rasterMapping <- terra::rasterize(polygonsMapping, targetGrid, field = "cellId")
 
   xyMapping <- terra::as.data.frame(rasterMapping, xy = TRUE)
 
-  xyTarget <- terra::crds(xTarget, df = TRUE, na.all = TRUE)
+  xyTarget <- terra::crds(targetGrid, df = TRUE, na.all = TRUE)
   xyTarget$source <- "target"
 
   mapAllInput <- merge(xyMapping, xyTarget, by = c("x", "y"), all.x = TRUE)
@@ -82,7 +87,7 @@ toolResolutionMapping <- function(mapping, xTarget) {
                                      "% of target cells missing in mapping, ",
                                      "adding those to mapping (nearest neighbor)"))
 
-    near <- terra::nearest(terra::vect(missingInMapping, geom = c("x", "y"), crs = terra::crs(xTarget)),
+    near <- terra::nearest(terra::vect(missingInMapping, geom = c("x", "y"), crs = terra::crs(targetGrid)),
                            pointsMapping)
     toolStatusMessage("warn", paste0("nearest neighbor distances: ",
                                      "max = ", round(max(near$distance) / 1000, 1), "km",
@@ -93,7 +98,7 @@ toolResolutionMapping <- function(mapping, xTarget) {
     near <- as.data.frame(near)
     colnames(near)[colnames(near) == "to_id"] <- "cellId"
 
-    mappingAddition <- merge(near, mapping)
+    mappingAddition <- merge(near, mapping, by = "cellId")
     mappingAddition$x <- mappingAddition$from_x
     mappingAddition$y <- mappingAddition$from_y
     mappingAddition <- mappingAddition[, mappingColumns]
@@ -103,12 +108,14 @@ toolResolutionMapping <- function(mapping, xTarget) {
     mappingAddition <- mapping[integer(0), ]
   }
 
-  colnames(mapping) <- sub("^x$", "xOriginal", colnames(mapping))
-  colnames(mapping) <- sub("^y$", "yOriginal", colnames(mapping))
+  mapping$cellOriginal <- paste0(sub("\\.", "p", mapping$x), ".", sub("\\.", "p", mapping$y))
+  mapping <- mapping[, setdiff(colnames(mapping), c("x", "y"))]
 
+  # removing cells which are not in the target dataset here
   mappingBase <- merge(mapAllTarget[!is.na(mapAllTarget$cellId), ], mapping, by = "cellId")
-  mappingColumns <- setdiff(mappingColumns, "cellId")
+
   result <- rbind(mappingBase[, mappingColumns], mappingAddition[, mappingColumns])
+  result$cell <- paste0(sub("\\.", "p", result$x), ".", sub("\\.", "p", result$y))
 
   stopifnot(setequal(paste(result$x, result$y), paste(xyTarget$x, xyTarget$y)))
   return(result)
