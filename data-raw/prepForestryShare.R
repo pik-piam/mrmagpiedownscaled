b58f129844fa3c7fa3b069745ef2be4de3b47d08
Pascal Sauer <pascal.sauer@pik-potsdam.de>
2024-06-28 16:19:19 +0200

    fix forestry share

--- data-raw/prepForestryShare.R
+++ data-raw/prepForestryShare.R
@@ -2,18 +2,18 @@ withr::with_package("mrcommons", {
   x <- calcOutput("LanduseInitialisation", cellular = TRUE, cells = "lpjcell",
                   selectyears = seq(1995, 2015, 5), aggregate = FALSE)
   x <- x[, , c("forestry", "secdforest")]
-  forestrySecdforest <- dimSums(x)
-  x <- x[, , "forestry"] / forestrySecdforest # calc the share forestry / (forestry + secdforest)
-  x[forestrySecdforest == 0] <- 0 # replace expected NAs with 0
   x <- collapseDim(x, "iso")
 
   # map from 67k magpie/lpjml cells to LUH 0.25deg cells
   withr::with_package("mrdownscale", {
     resolutionMapping <- calcOutput("ResolutionMapping", aggregate = FALSE)
   })
-
   x <- x[getItems(x, 1) %in% resolutionMapping$cellOriginal, , ]
   x <- toolAggregate(x, resolutionMapping, from = "cellOriginal", to = "cell")
+
+  forestrySecdforest <- dimSums(x)
+  x <- x[, , "forestry"] / forestrySecdforest # calc the share forestry / (forestry + secdforest)
+  x[forestrySecdforest == 0] <- 0 # replace expected NAs with 0
 
   stopifnot(!is.na(x))
   write.magpie(x, "inst/extdata/forestryShare.mz")
