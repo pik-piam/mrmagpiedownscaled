9a896f0613f80b66c48413e4ae7da737cb55365a
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2024-08-09 10:28:20 +0200

    restructured fullESM and moved calculations to calc-functions

--- R/fullESM.R
+++ R/fullESM.R
@@ -8,8 +8,6 @@
 #' When called via madrat::retrieveData rev will be converted to numeric_version.
 #' @param ... reserved for future use
 #' @param scenario scenario name to be included in filenames
-#' @param years vector of min and max year specifying the range of years for which
-#' years will be returned. Years outside of this range will be ignored.
 #' @param harmonizationPeriod Two integer values, before the first given
 #' year the target dataset is used, after the second given year the input
 #' dataset is used, in between harmonize between the two datasets
@@ -18,176 +16,37 @@
 #' @param progress boolean defining whether progress should be printed
 #'
 #' @author Pascal Sauer, Jan Philipp Dietrich
-fullESM <- function(rev = NULL, ..., scenario = "", years = c(2020, 2100),
-                    harmonizationPeriod = c(2015, 2050), compression = 2, progress = TRUE) {
+fullESM <- function(rev = NULL, ..., scenario = "", harmonizationPeriod = c(2015, 2050),
+                    compression = 2, progress = TRUE) {
+
   stopifnot(...length() == 0)
-  missingValue <- 1e20
-  resolution <- 0.25
-  gridDefinition <- c(-179.875, 179.875, -89.875, 89.875, resolution)
 
   revision <- if (is.null(rev)) format(Sys.time(), "%Y-%m-%d") else rev
+
   fileSuffix <- paste0("_input4MIPs_landState_RESCUE_PIK-MAgPIE-4-7-",
                        scenario, if (scenario != "") "-",
-                       revision, "_gn_", min(years), "-", max(years))
+                       revision, "_gn_2020-2100.nc")
 
-  land <- calcOutput("LandReport", outputFormat = "ESM",
-                     harmonizationPeriod = harmonizationPeriod, aggregate = FALSE)
-  land <- adaptYearsESM(land, years)
+  writeArgs <- list(compression = compression, missval = 1e20, progress = progress,
+                    gridDefinition = c(-179.875, 179.875, -89.875, 89.875, 0.25))
 
-  statesFile <- paste0("multiple-states", fileSuffix, ".nc")
-  statesVariables <- c("c3ann", "c3nfx", "c3per", "c4ann", "c4per", "pastr",
-                       "primf", "primn", "range", "secdf", "secdn", "urban")
-  write.magpie(land[, , statesVariables], statesFile, compression = compression,
-               missval = missingValue, gridDefinition = gridDefinition, progress = progress)
-  addMetadataESM(statesFile, revision, missingValue, resolution, compression, harmonizationPeriod)
+  metadataArgs <- list(revision = revision, missingValue = 1e20, resolution = 0.25,
+                       compression = compression, harmonizationPeriod = harmonizationPeriod)
 
-  landManagementVariables <- c("irrig_c3ann", "crpbf_c3ann", "irrig_c3nfx", "crpbf_c3nfx",
-                               "irrig_c3per", "crpbf_c3per", "crpbf2_c3per", "irrig_c4ann",
-                               "crpbf_c4ann", "irrig_c4per", "crpbf_c4per", "crpbf2_c4per", "manaf")
-  land <- land[, , landManagementVariables]
-  nonland <- calcOutput("NonlandReport", outputFormat = "ESM",
-                        harmonizationPeriod = harmonizationPeriod, aggregate = FALSE)
-  nonland <- adaptYearsESM(nonland, years)
-  nonlandManagementVariables <- c("fertl_c3nfx", "fertl_c3per", "fertl_c3ann", "fertl_c4ann",
-                                  "fertl_c4per", "rndwd", "fulwd")
-  management <- mbind(land, nonland[, , nonlandManagementVariables])
-  rm(land)
-  managementFile <- paste0("multiple-management", fileSuffix, ".nc")
-  write.magpie(management, managementFile, compression = compression,
-               missval = missingValue, gridDefinition = gridDefinition, progress = progress)
-  rm(management)
-  addMetadataESM(managementFile, revision, missingValue, resolution, compression, harmonizationPeriod)
+  file <- paste0("multiple-states", fileSuffix)
+  calcOutput("ESMStates", harmonizationPeriod = harmonizationPeriod,
+             aggregate = FALSE, file = file, writeArgs = writeArgs)
+  toolAddMetadataESM(file, metadataArgs)
 
-  woodSources <- c("primf", "secyf", "secmf", "primn", "secnf")
-  woodHarvestVariables <- c(paste0(woodSources, "_bioh"), paste0(woodSources, "_harv"))
-  nonland <- nonland[, , woodHarvestVariables]
+  file <- paste0("multiple-management", fileSuffix)
+  calcOutput("ESMManagement", harmonizationPeriod = harmonizationPeriod,
+             aggregate = FALSE, file = file, writeArgs = writeArgs)
+  toolAddMetadataESM(file, metadataArgs)
 
-  transitions <- calcOutput("LandTransitions", outputFormat = "ESM",
-                            harmonizationPeriod = harmonizationPeriod, aggregate = FALSE)
-  getItems(transitions, raw = TRUE, dim = 3) <- sub("\\.", "_to_", getItems(transitions, dim = 3))
-  getSets(transitions, fulldim = FALSE)[3] <- "transitions"
-
-  # we use to-semantics for transitions (value for 1994 describes what happens from 1993 to 1994)
-  # by subtracting 1 we get from-semantics (value for 1994 describes what happens from 1994 to 1995)
-  # which is what LUH uses
-  getYears(transitions) <- getYears(transitions, as.integer = TRUE) - 1
-  transitions <- adaptYearsESM(transitions, years)
-  transitions <- mbind(transitions, nonland)
-  rm(nonland)
-  transitionsFile <- paste0("multiple-transitions", fileSuffix, ".nc")
-  write.magpie(transitions, transitionsFile, compression = compression,
-               missval = missingValue, gridDefinition = gridDefinition, progress = progress)
-  rm(transitions)
-  addMetadataESM(transitionsFile, revision, missingValue, resolution, compression, harmonizationPeriod)
+  file <- paste0("multiple-transitions", fileSuffix)
+  calcOutput("ESMTransitions", harmonizationPeriod = harmonizationPeriod,
+             aggregate = FALSE, file = file, writeArgs = writeArgs)
+  toolAddMetadataESM(file, metadataArgs)
 
   toolWriteMadratLog(logPath = "consistencyCheck.log")
-}
-
-adaptYearsESM <- function(x, years) {
-  x <- x[, getYears(x, as.integer = TRUE) %in% min(years):max(years), ]
-  # account for unit "years since 1970-01-01 0:0:0" which addMetadataESM sets
-  x <- setYears(x, getYears(x, as.integer = TRUE) - 1970)
-  return(x)
-}
-
-addMetadataESM <- function(ncFile, revision, missingValue, resolution, compression, harmonizationPeriod) {
-  variableId <- sub("^(multiple-[^_]+).+$", "\\1", basename(ncFile))
-  stopifnot(variableId %in% c("multiple-states", "multiple-management", "multiple-transitions"))
-  nc <- ncdf4::nc_open(ncFile, write = TRUE)
-  withr::defer({
-    ncdf4::nc_close(nc)
-  })
-  # global
-  dateTime <- strftime(Sys.time(), format = "%Y-%m-%dT%H:%M:%SZ", tz = "UTC")
-  ncdf4::ncatt_put(nc, 0, "activity_id", "RESCUE/OptimESM")
-  ncdf4::ncatt_put(nc, 0, "contact", "pascal.sauer@pik-potsdam.de, dietrich@pik-potsdam.de")
-  ncdf4::ncatt_put(nc, 0, "Conventions", "CF-1.6")
-  ncdf4::ncatt_put(nc, 0, "creation_date", dateTime)
-  ncdf4::ncatt_put(nc, 0, "data_structure", "grid")
-  ncdf4::ncatt_put(nc, 0, "dataset_category", "landState")
-  ncdf4::ncatt_put(nc, 0, "dataset_version_number", as.character(revision))
-  ncdf4::ncatt_put(nc, 0, "date", dateTime)
-  ncdf4::ncatt_put(nc, 0, "frequency", "yr")
-  ncdf4::ncatt_put(nc, 0, "further_info_url", "https://github.com/pik-piam/mrdownscale/blob/main/runner/changelog.md")
-  ncdf4::ncatt_put(nc, 0, "grid_label", "gn")
-  ncdf4::ncatt_put(nc, 0, "host", "Potsdam Institute for Climate Impact Research")
-  ncdf4::ncatt_put(nc, 0, "institution_id", "PIK")
-  ncdf4::ncatt_put(nc, 0, "institution", "Potsdam Institute for Climate Impact Research")
-  ncdf4::ncatt_put(nc, 0, "license", "CC BY 4.0")
-  ncdf4::ncatt_put(nc, 0, "nominal_resolution", "50 km")
-  ncdf4::ncatt_put(nc, 0, "realm", "land")
-  ncdf4::ncatt_put(nc, 0, "references",
-                   "https://github.com/pik-piam/mrdownscale and https://rescue-climate.eu/ and https://optimesm-he.eu/")
-  ncdf4::ncatt_put(nc, 0, "source_id", sub(paste0("^", variableId, "_"), "",
-                                           sub("\\.nc$", "",
-                                               basename(ncFile))))
-  ncdf4::ncatt_put(nc, 0, "target_mip", "RESCUE/OptimESM")
-  ncdf4::ncatt_put(nc, 0, "title", "MAgPIE Land-Use Data Harmonized and Downscaled using LUH2 v2h as reference")
-  ncdf4::ncatt_put(nc, 0, "variable_id", variableId)
-
-  # added by us
-  ncdf4::ncatt_put(nc, 0, "harmonization_period", paste(harmonizationPeriod, collapse = "-"))
-  ncdf4::ncatt_put(nc, 0, "harmonization_downscaling_tool", "https://github.com/pik-piam/mrdownscale")
-  ncdf4::ncatt_put(nc, 0, "reference_dataset", "LUH2 v2h Release (10/14/16) from https://luh.umd.edu/data.shtml")
-  ncdf4::ncatt_put(nc, 0, "source_version", as.character(revision))
-
-  # time
-  ncdf4::ncatt_put(nc, "time", "axis", "T")
-  ncdf4::ncatt_put(nc, "time", "calendar", "365_day")
-  ncdf4::ncatt_put(nc, "time", "long_name", "time")
-  ncdf4::ncatt_put(nc, "time", "realtopology", "linear")
-  ncdf4::ncatt_put(nc, "time", "standard_name", "time")
-  ncdf4::ncatt_put(nc, "time", "units", "years since 1970-01-01 0:0:0")
-
-  # lon
-  ncdf4::ncatt_put(nc, "lon", "realtopology", "circular")
-  ncdf4::ncatt_put(nc, "lon", "topology", "circular")
-  ncdf4::ncatt_put(nc, "lon", "modulo", 360, prec = "double")
-  ncdf4::ncatt_put(nc, "lon", "long_name", "longitude")
-  ncdf4::ncatt_put(nc, "lon", "standard_name", "longitude")
-  ncdf4::ncatt_put(nc, "lon", "axis", "X")
-  ncdf4::ncatt_put(nc, "lon", "bounds", "bounds_lon")
-
-  # lat
-  ncdf4::ncatt_put(nc, "lat", "realtopology", "linear")
-  ncdf4::ncatt_put(nc, "lat", "long_name", "latitude")
-  ncdf4::ncatt_put(nc, "lat", "standard_name", "latitude")
-  ncdf4::ncatt_put(nc, "lat", "axis", "Y")
-  ncdf4::ncatt_put(nc, "lat", "bounds", "bounds_lat")
-
-  # variable attributes
-  luhNames <- toolGetMapping("luhNames.csv", where = "mrdownscale")
-  for (varname in names(nc$var)) {
-    ncdf4::ncatt_put(nc, varname, "_Fillvalue", missingValue, prec = "float")
-    ncdf4::ncatt_put(nc, varname, "missing_value", missingValue, prec = "float")
-    ncdf4::ncatt_put(nc, varname, "cell_methods", "time:mean")
-
-    varnameLuhNames <- as.vector(luhNames[luhNames$name == varname, ])
-    ncdf4::ncatt_put(nc, varname, "units", varnameLuhNames$unit)
-    ncdf4::ncatt_put(nc, varname, "long_name", varnameLuhNames$long_name)
-    ncdf4::ncatt_put(nc, varname, "standard_name", varnameLuhNames$standard_name)
-    if (varnameLuhNames$standard_name_description != "") {
-      ncdf4::ncatt_put(nc, varname, "standard_name_description", varnameLuhNames$standard_name_description)
-    }
-    if (varnameLuhNames$long_name_description != "") {
-      ncdf4::ncatt_put(nc, varname, "long_name_description", varnameLuhNames$long_name_description)
-    }
-  }
-
-  # add bounds
-  boundsDim <- ncdf4::ncdim_def("bounds", "", 1:2, create_dimvar = FALSE)
-
-  withr::with_options(list(warnPartialMatchDollar = FALSE), {
-    nc <- ncdf4::ncvar_add(nc, ncdf4::ncvar_def("bounds_lon", units = "",
-                                                dim = list(boundsDim, nc$dim$lon),
-                                                prec = "double", compression = compression))
-    nc <- ncdf4::ncvar_add(nc, ncdf4::ncvar_def("bounds_lat", units = "",
-                                                dim = list(boundsDim, nc$dim$lat),
-                                                prec = "double", compression = compression))
-  })
-
-  ncdf4::ncvar_put(nc, "bounds_lon", rbind(nc$dim$lon$vals - resolution / 2,
-                                           nc$dim$lon$vals + resolution / 2))
-  ncdf4::ncvar_put(nc, "bounds_lat", rbind(nc$dim$lat$vals + resolution / 2,
-                                           nc$dim$lat$vals - resolution / 2))
 }
