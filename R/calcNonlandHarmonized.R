c32b5941d52aa1cf243694f78623d1476107d21a
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2024-08-20 13:08:37 +0200

    pull out wood harvest area check/fix

--- R/calcNonlandHarmonized.R
+++ R/calcNonlandHarmonized.R
@@ -81,6 +81,13 @@ calcNonlandHarmonized <- function(input = "magpie", target = "luh2mod",
     }
   }
 
+  categories <- grep("wood_harvest_area$", getItems(out, 3), value = TRUE)
+  out[, , categories] <- toolWoodHarvestArea(out[, , categories],
+                                             calcOutput("LandHarmonized", input = input, target = target,
+                                                        harmonizationPeriod = harmonizationPeriod,
+                                                        method = method, aggregate = FALSE),
+                                             fix = TRUE)
+
   attr(out, "geometry") <- geometry
   attr(out, "crs")      <- crs
 
@@ -95,27 +102,51 @@ calcNonlandHarmonized <- function(input = "magpie", target = "luh2mod",
   # SpatRaster can hold values up to ~10^40 before replacing with Inf, so check we are well below that
   toolExpectTrue(max(out) < 10^30, "All values are < 10^30")
 
-  # check wood harvest area * time step length (as unit is Mha yr-1) <=
-  # land of the correponding type (in the previous timestep)
-  land <- calcOutput("LandHarmonized", aggregate = FALSE)
-  woodHarvestArea <- out[, , sort(grep("wood_harvest_area$", getNames(out), value = TRUE))]
-  stopifnot(identical(getItems(woodHarvestArea, 3),
-                      paste0(c("primf", "primn", "secmf", "secnf", "secyf"),
-                             "_wood_harvest_area")))
-  getItems(woodHarvestArea, 3) <- c("primf", "primn", "secdf", "secdn", "secdf")
-  stopifnot(identical(getYears(woodHarvestArea), getYears(land)),
-            getItems(woodHarvestArea, 3) %in% getItems(land, 3))
-  years <- getYears(land, as.integer = TRUE)
-  timestepLengths <- new.magpie(years = years[-1], fill = diff(years))
-  woodland <- setYears(land[, -nyears(land), getItems(woodHarvestArea, 3)], years[-1])
-  landOvershoot <- min(woodland - timestepLengths * collapseDim(woodHarvestArea)[, -1, ])
-  toolExpectTrue(landOvershoot >= -10^-10, paste0("Wood harvest area is smaller than land ",
-                                                  "of the corresponding type ",
-                                                  "(maxdiff: ", landOvershoot, ")"))
-
   return(list(x = out,
               isocountries = FALSE,
               unit = "harvest_weight & bioh: kg C yr-1; harvest_area: Mha yr-1; fertilizer: kg yr-1",
               min = 0,
               description = "Harmonized nonland data"))
+}
+
+# cap wood harvest area at land area of the corresponding type (in the previous timestep) / timestep length
+toolWoodHarvestArea <- function(x, land, fix) {
+  map <- as.data.frame(rbind(c("primf_wood_harvest_area", "primf"),
+                             c("secyf_wood_harvest_area", "secdf"),
+                             c("secmf_wood_harvest_area", "secdf"),
+                             c("primn_wood_harvest_area", "primn"),
+                             c("secnf_wood_harvest_area", "secdn")))
+  colnames(map) <- c("harvest", "land")
+
+  stopifnot(setequal(getItems(x, 3), map$harvest))
+
+  woodHarvestAreaBefore <- x
+  woodHarvestArea <- woodHarvestAreaBefore
+  woodHarvestArea <- toolAggregate(woodHarvestArea, map, from = "harvest", to = "land", dim = 3)
+
+  stopifnot(identical(getYears(woodHarvestArea), getYears(land)),
+            getItems(woodHarvestArea, 3) %in% getItems(land, 3))
+  years <- getYears(land, as.integer = TRUE)
+  timestepLengths <- new.magpie(years = years[-1], fill = diff(years))
+  woodland <- setYears(land[, -nyears(land), getItems(woodHarvestArea, 3)], years[-1])
+
+  if (fix) {
+    woodHarvestArea[, -1, ] <- pmin(woodHarvestArea[, -1, ], woodland / timestepLengths)
+    x <- toolAggregate(woodHarvestArea, map, weight = woodHarvestAreaBefore + 10^-10,
+                       from = "land", to = "harvest", dim = 3)
+
+    maxCorrection <- max(abs(woodHarvestAreaBefore - x))
+    if (maxCorrection > 10^-10) {
+      toolStatusMessage("note", paste("wood harvest area was reduced to not exceed land area",
+                                      "(max correction: ", signif(maxCorrection, 3), " Mha yr-1)"),
+                        level = 1)
+    }
+  }
+
+  landOvershoot <- min(woodland - timestepLengths * collapseDim(woodHarvestArea)[, -1, ])
+  toolExpectTrue(landOvershoot >= -10^-10, paste0("Wood harvest area is smaller than land ",
+                                                  "of the corresponding type ",
+                                                  "(max overshoot: ", signif(-landOvershoot, 3), " Mha)"),
+                 level = 1)
+  return(x)
 }
