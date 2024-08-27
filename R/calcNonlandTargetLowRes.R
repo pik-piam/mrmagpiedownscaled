f48b9bbe01bb2dac839640d637b020e8b82f365d
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2024-08-27 10:35:38 +0200

    some checks

--- R/calcNonlandTargetLowRes.R
+++ R/calcNonlandTargetLowRes.R
@@ -9,11 +9,16 @@ calcNonlandTargetLowRes <- function(input = "magpie", target = "luh2mod") {
   xTarget <- as.magpie(xTarget)
 
   stopifnot(setequal(getItems(xInput, 3), getItems(xTarget, 3)))
-  xTarget <- xTarget[, , getItems(xInput, 3)] # harmonize order of dim 3
+  out <- xTarget[, , getItems(xInput, 3)] # harmonize order of dim 3
 
-  # TODO checks
+  woody <- c("primf", "secyf", "secmf", "primn", "secnf")
+  roundFuelWood <- c("roundwood_harvest_weight_type", "fuelwood_harvest_weight_type")
+  toolExpectLessDiff(dimSums(out[, , paste0(woody, "_bioh")], 3),
+                     dimSums(out[, , roundFuelWood], 3),
+                     10^5, "Harvest weight types are consistent")
+  toolExpectTrue(min(out) >= 0, "All values are >= 0")
 
-  return(list(x = xTarget,
+  return(list(x = out,
               isocountries = FALSE,
               unit = "harvest_weight & bioh: kg C yr-1; harvest_area: Mha yr-1; fertilizer: kg yr-1",
               min = 0,
